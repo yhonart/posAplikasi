ae2382b0ef329fcadc4de209e5471e0f6c775626
yhONdev <yusuf7789@gmail.com>
2024-09-24 14:41:01 +0700

    update nama toko

--- resources/views/layouts/login.blade.php
+++ resources/views/layouts/login.blade.php
@@ -22,7 +22,7 @@
 <body class="hold-transition login-page">
     <div class="login-box">
         <div class="login-logo">
-            <a href="../../index2.html"><b>POS</b></a>
+            <a href="../../index2.html">Toko<b>LingLing</b></a>
         </div>        
         @yield('content')
     </div>
