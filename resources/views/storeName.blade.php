1b52e86839f48cc55e50e793bbbf7c11e92726ff
yhONdev <yusuf7789@gmail.com>
2024-09-25 09:07:38 +0700

    change layout sidebar

--- resources/views/storeName.blade.php
+++ resources/views/storeName.blade.php
@@ -1,1 +1,5 @@
-<span class="brand-text font-weight-bold">{{$storeName->company_name}}</span>
+@if($storeName->company_name <> '')
+<span class="brand-text font-weight-bold"><i class="fa-solid fa-store"></i> {{$storeName->company_name}}</span>
+@else
+<span class="brand-text font-weight-bold"><i class="fa-solid fa-store"></i> TOKO ID</span>
+@endif
