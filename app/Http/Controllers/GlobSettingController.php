8ca6b2c3c7bba81905b8e93d61c50e2bc97d3c40
yhONdev <yusuf7789@gmail.com>
2024-10-02 15:30:21 +0700

    update

--- app/Http/Controllers/GlobSettingController.php
+++ app/Http/Controllers/GlobSettingController.php
@@ -152,7 +152,7 @@ class GlobSettingController extends Controller
         
         //Ambil data dengan sizecode selain 1
         $sizeCodeDua = DB::table('m_product_unit')
-        ->select('idm_product_satuan','product_price_order','size_code','core_id_product')
+        ->select('idm_product_satuan','product_price_order','size_code','core_id_product','product_volume')
         ->where([
             ['size_code','!=','1'],
             ['core_id_product','228']
