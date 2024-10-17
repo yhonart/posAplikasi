611fd5dd4e8a2caa6267360e4806112849ddc40b
yhONdev <yusuf7789@gmail.com>
2024-10-17 16:18:15 +0700

    add core id product

--- resources/views/Stock/MasterData/stockFormNewGroupListPrd.blade.php
+++ resources/views/Stock/MasterData/stockFormNewGroupListPrd.blade.php
@@ -18,7 +18,7 @@
                 <td class="p-0"><b>{{$mPrd->product_satuan}}/</b><small>{{$mPrd->product_size}}</small></td>
                 <td class="p-0">
                     @if($mPrd->size_code == '1')
-                        <input class="form-control form-control-sm rounded-0 EDIT-PRICE" name="editPriceOrder" id="eeditPriceOrder" value="{{$mPrd->product_price_order}}" onchange="saveToDatabase(this,'m_product_unit','product_price_order','{{$mPrd->idm_product_satuan}}','idm_product_satuan')">
+                        <input class="form-control form-control-sm rounded-0 EDIT-PRICE" name="editPriceOrder" id="eeditPriceOrder" value="{{$mPrd->product_price_order}}" onchange="saveToDatabase(this,'m_product_unit','product_price_order','{{$mPrd->idm_product_satuan}}','idm_product_satuan','{{$mPrd->core_id_product}}')">
                     @else
                         <input class="form-control form-control-sm text-right rounded-0" value="{{$mPrd->product_price_order}}" readonly>
                     @endif
@@ -27,7 +27,7 @@
                     <td class="p-0">
                         @foreach($groupProdList as $gP)
                             @if( $gP->cos_group == $mG1->idm_cos_group AND $gP->size_product == $mPrd->product_size)
-                                <input type="text" class="form-control form-control-sm rounded-0 EDIT-PRICE" name="editPriceSell" id="editPriceSell" value="{{$gP->price_sell}}" onchange="saveToDatabase(this,'m_product_price_sell','price_sell','{{$gP->idm_price_sell}}','idm_price_sell')">
+                                <input type="text" class="form-control form-control-sm rounded-0 EDIT-PRICE" name="editPriceSell" id="editPriceSell" value="{{$gP->price_sell}}" onchange="saveToDatabase(this,'m_product_price_sell','price_sell','{{$gP->idm_price_sell}}','idm_price_sell','{{$mPrd->core_id_product}}')">
                             @endif
                         @endforeach
                     </td>
