bf5d10eb8aeab8cf7c7bd0d09cf492d9fedd8363
yhONdev <yusuf7789@gmail.com>
2024-11-08 14:12:07 +0700

    change datatable download

--- resources/views/TrxKasUmum/listTransactionKas.blade.php
+++ resources/views/TrxKasUmum/listTransactionKas.blade.php
@@ -54,15 +54,10 @@
 </div>
 <script>
     $(function () {
-        $('#tableTrxKas').DataTable({
-          "paging": true,
-          "lengthChange": true,
-          "searching": true,
-          "ordering": true,
-          "info": true,
-          "autoWidth": false,
-          "responsive": true,
-        });
+        $("#tableTrxKas").DataTable({
+        "responsive": true, "lengthChange": false, "autoWidth": false,
+        "buttons": ["copy", "csv", "excel", "pdf", "print", "colvis"]
+        }).buttons().container().appendTo('#example1_wrapper .col-md-6:eq(0)');        
     });
     $(document).ready(function(){
         $(".dataTable").on('click','.EDIT-KAS', function () {
