50bd3cafaee6b40044760b4bad4f5748e3e419fe
yhONdev <yusuf7789@gmail.com>
2024-11-08 10:53:12 +0700

    config edit kas

--- resources/views/TrxKasUmum/editTransaksiKas.blade.php
+++ resources/views/TrxKasUmum/editTransaksiKas.blade.php
@@ -9,14 +9,14 @@
                     <div class="form-group row">
                         <label class="label col-md-4">Tanggal</label>
                         <div class="col-md-4">
-                            <input type="text" class="form-control form-control-sm rounded-0 modalDate-input" name="tanggal" id="tanggal">
+                            <input type="text" class="form-control form-control-sm rounded-0 modalDate-input" name="tanggal" id="tanggal" value="{{$editData->kas_date}}">
                         </div>
                     </div>
                     <div class="form-group row">
                         <label class="label col-md-4">Kategori</label>
                         <div class="col-md-4">
                             <select name="selKategori" id="selKategori" class="form-control form-control-sm rounded-0 select-2">
-                                <option value="0">===</option>
+                                <option value="0">{{$editData->cat_name}}</option>
                                 @foreach($kasKategori as $kk)
                                     <option value="{{$kk->idm_cat_kas}}">{{$kk->cat_name}}</option>
                                 @endforeach
@@ -27,21 +27,21 @@
                         <label class="label col-md-4">Sub-Kategori</label>
                         <div class="col-md-4">
                             <select name="subKategori" id="subKategori" class="form-control form-control-sm rounded-0 select-2">
-                                <option value="0">===</option>
+                                <option value="0">{{$editData->subcat_name}}</option>
                             </select>
                         </div>
                     </div>
                     <div class="form-group row">
                         <label class="label col-md-4">Keterangan</label>
                         <div class="col-md-4">
-                            <input type="text" class="form-control form-control-sm rounded-0" name="keterangan" id="keterangan">
+                            <input type="text" class="form-control form-control-sm rounded-0" name="keterangan" id="keterangan" value="{{$editData->description}}">
                         </div>
                     </div>
                     <div class="form-group row">
                         <label class="label col-md-4">Personal</label>
                         <div class="col-md-4">
                             <select name="personal" id="personal" class="form-control form-control-sm rounded-0 select-2">
-                                <option value="0"></option>
+                                <option value="0">{{$editData->kas_persName}}</option>
                                 @foreach($mStaff as $ms)
                                 <option value="{{$ms->sales_code}}|{{$ms->sales_name}}">{{$ms->sales_name}} (Sales)</option>
                                 @endforeach
@@ -54,11 +54,15 @@
                     <div class="form-group row">
                         <label class="label col-md-4">nominal</label>
                         <div class="col-md-4">
-                            <input type="text" class="form-control form-control-sm rounded-0" name="nominal" id="nominal">
+                            <input type="text" class="form-control form-control-sm rounded-0" name="nominal" id="nominal" value="{{$editData->description}}">
                         </div>
                     </div>
                     <div class="form-group row">
                         <label class="label col-md-4">Lampiran</label>
+                        <div class="col-md-3">
+                            <a href="#" id="deleteLamp"><i class="fa-solid fa-trash-can"></i></a>
+                            <a href="{{asset('public/images/Upload/TrxKas')}}/{{$editData->file_name}}" target="_blank" rel="noopener noreferrer">{{$editData->file_name}}</a>
+                        </div>
                         <div class="col-md-4">
                             <input type="file" name="docLampiran" id="docLampiran" class="form-control-file">
                         </div>
