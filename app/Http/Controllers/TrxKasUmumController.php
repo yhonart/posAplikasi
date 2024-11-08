b28da95bb6b42700322f048584ed9e6426a455dc
yhONdev <yusuf7789@gmail.com>
2024-11-08 08:20:45 +0700

    adding as in table

--- app/Http/Controllers/TrxKasUmumController.php
+++ app/Http/Controllers/TrxKasUmumController.php
@@ -91,10 +91,10 @@ class TrxKasUmumController extends Controller
 
     public function filterByDate($fromDate, $endDate)
     {
-        $displayByDate = DB::table('tr_kas a');
+        $displayByDate = DB::table('tr_kas as a');
             $displayByDate = $displayByDate->select('a.*','b.cat_name','c.subcat_name');
-            $displayByDate = $displayByDate->leftJoin('m_category_kas b','b.idm_cat_kas','=','a.kas_catId');
-            $displayByDate = $displayByDate->leftJoin('m_subcategory_kas c','c.idm_sub ','=','a.kas_subCatId');
+            $displayByDate = $displayByDate->leftJoin('m_category_kas as b','b.idm_cat_kas','=','a.kas_catId');
+            $displayByDate = $displayByDate->leftJoin('m_subcategory_kas as c','c.idm_sub ','=','a.kas_subCatId');
             $displayByDate = $displayByDate->where([
                 ['status','1']
             ]);
