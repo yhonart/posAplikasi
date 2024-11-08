f6aa6ae6529d8e9821d18b5d2e39b21905202feb
yhONdev <yusuf7789@gmail.com>
2024-11-08 14:52:06 +0700

    change display log

--- resources/views/Dashboard/modalDashListTrx.blade.php
+++ resources/views/Dashboard/modalDashListTrx.blade.php
@@ -1,5 +1,13 @@
 <?php
     $no = '1';
+    $arayStatus = array(
+        0=>"Trs. Batal",
+        1=>"Dlm. Proses",
+        2=>"Hold",
+        3=>"Kredit",
+        4=>"Trs. Sukses",
+    );
+    $total = '0';
 ?>
 <div class="row">
     <div class="col-md-12">
@@ -25,12 +33,30 @@
                             <td>{{$hisTrx->product_name}}</td>
                             <td>{{$hisTrx->qty}}</td>
                             <td>{{$hisTrx->unit}}</td>
-                            <td>{{$hisTrx->unit_price}}</td>
-                            <td>{{$hisTrx->disc}}</td>
-                            <td>{{$hisTrx->t_price}}</td>
-                            <td>{{$hisTrx->status}}</td>
+                            <td class="text-right">
+                                {{number_format($hisTrx->unit_price,'0',',','.')}}
+                            </td>
+                            <td class="text-right">
+                                {{number_format($hisTrx->disc,'0',',','.')}}
+                            </td>
+                            <td class="text-right">
+                                {{number_format($hisTrx->t_price,'0',',','.')}}
+                                <?php
+                                    $total += $hisTrx->t_price;
+                                ?>
+                            </td>
+                            <td class="text-right">
+                                <span class="bg-light border border-1 border-info pl-2 pr-2 pt-1 pb-1 rounded-pill font-weight-bold text-xs">
+                                    {{$arayStatus[$hisTrx->status]}}
+                                </span>
+                            </td>
                         </tr>
                     @endforeach
+                        <tr>
+                            <td colspan="6" class="bg-light font-weight-bold">Total </td>
+                            <td class="bg-light font-weight-bold">{{number_format($total,'0',',','.')}}</td>
+                            <td></td>
+                        </tr>
                 </tbody>
             </table>
         </div>
