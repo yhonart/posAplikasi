e6fb6102da934b5b866976192720a3c5799be3f2
yhONdev <yusuf7789@gmail.com>
2024-11-21 22:39:15 +0700

    add button edit

--- resources/views/HutangCustomers/UnlockCustomerLoan.blade.php
+++ resources/views/HutangCustomers/UnlockCustomerLoan.blade.php
@@ -12,7 +12,10 @@
             <tr>
                 <td>{{$cslt->customer_code}}</td>
                 <td>{{$cslt->customer_store}}</td>
-                <td>{{number_format($cslt->kredit_limit,'0',',','.')}}</td>
+                <td class="text-right">
+                    {{number_format($cslt->kredit_limit,'0',',','.')}}
+                    <button type="button" id="editLimit" class="btn btn-sm btn-default border-0"><i class="fa-solid fa-file-pen"></i></button>
+                </td>
                 <td>
                 <div class="form-group">
                     <div class="custom-control custom-switch">
