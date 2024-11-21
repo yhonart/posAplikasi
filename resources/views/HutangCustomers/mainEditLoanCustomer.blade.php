d70c31cb55b8054ac74b82d8b15ee025841ab1d0
yhONdev <yusuf7789@gmail.com>
2024-11-21 16:51:20 +0700

    changes layout admin pinjaman

--- resources/views/HutangCustomers/mainEditLoanCustomer.blade.php
+++ resources/views/HutangCustomers/mainEditLoanCustomer.blade.php
@@ -1,57 +1,74 @@
-<div class="row p-1">
-    <div class="col-12">
-        <div class="card card-purple">
-            <div class="card-header border-0">
-                <h3 class="card-title font-weight-bold">Data Pinjaman & Pelunasan</h3>
-                <div class="card-tools">
-                    <button type="button" class="btn btn-tool border-0 elevation-1" data-dismiss="modal"><i class="fa-solid fa-xmark"></i></button>
-                </div>
-            </div>
-            <div class="card-body">                
-                <div class="row">
-                    <div class="col-6 col-md-3">
-                        <div class="form-group">
-                           <select name="actionCode" id="actionCode" class="from-control form-control-sm">
-                            <option value="0">Pilih Proses </option>
-                            <option value="1">Pembayaran Hutan</option>
-                            <option value="2">Open Transaksi Hutang</option>
-                            <option value="3">Edit Limit Hutang</option>
-                           </select>
-                        </div>
-                    </div>                    
-                    <div class="col-12 col-md-3">
-                        <div class="form-group">
-                            <label class="form-label">Cari</label>
-                            <select class="form-control form-control-sm" id="cariNamaPelanggan" class="form-control form-control-sm select-pelanggan">
-                                <option value="0" readonly>Nama Pelanggan</option>
-                                @foreach($dbMCustomer as $dcs)
-                                <option value="{{$dcs->idm_customer }}">{{$dcs->customer_store}}</option>
-                                @endforeach
-                            </select>
+@extends('layouts.sidebarpage')
+@section('content')
+<!-- Header -->
+<div class="content-header">
+    <div class="container-fluid">
+        <div class="row mb-2">
+            <div class="col-md-8 col-12">          
+                <h1 class="m-0">Laporan Inventory</h1>
+            </div>          
+        </div>
+    </div>
+</div>
+<div class="content mt-0">
+    <div class="container-fluid">
+        <div class="row p-1">
+            <div class="col-12">
+                <div class="card card-purple">
+                    <div class="card-header border-0">
+                        <h3 class="card-title font-weight-bold">Data Pinjaman & Pelunasan</h3>
+                        <div class="card-tools">
+                            <button type="button" class="btn btn-tool border-0 elevation-1" data-dismiss="modal"><i class="fa-solid fa-xmark"></i></button>
                         </div>
                     </div>
-                    <div class="col-6 col-md-3">
-                        <div class="form-group">
-                            <label class="form-label">Dari Tanggal</label>
-                            <input type="text" class="form-control datetimepicker-input  from-control-sm" name="dariTanggal" id="dariTanggal">
+                    <div class="card-body">                
+                        <div class="row">
+                            <div class="col-6 col-md-3">
+                                <div class="form-group">
+                                <select name="actionCode" id="actionCode" class="from-control form-control-sm">
+                                    <option value="0">Pilih Proses </option>
+                                    <option value="1">Pembayaran Hutan</option>
+                                    <option value="2">Open Transaksi Hutang</option>
+                                    <option value="3">Edit Limit Hutang</option>
+                                </select>
+                                </div>
+                            </div>                    
+                            <div class="col-12 col-md-3">
+                                <div class="form-group">
+                                    <label class="form-label">Cari</label>
+                                    <select class="form-control form-control-sm" id="cariNamaPelanggan" class="form-control form-control-sm select-pelanggan">
+                                        <option value="0" readonly>Nama Pelanggan</option>
+                                        @foreach($dbMCustomer as $dcs)
+                                        <option value="{{$dcs->idm_customer }}">{{$dcs->customer_store}}</option>
+                                        @endforeach
+                                    </select>
+                                </div>
+                            </div>
+                            <div class="col-6 col-md-3">
+                                <div class="form-group">
+                                    <label class="form-label">Dari Tanggal</label>
+                                    <input type="text" class="form-control datetimepicker-input  from-control-sm" name="dariTanggal" id="dariTanggal">
+                                </div>
+                            </div>
+                            <div class="col-6 col-md-3">
+                                <div class="form-group">
+                                    <label class="form-label">s.d Tanggal</label>
+                                    <input type="text" class="form-control datetimepicker-input roundedd-0 form-control-sm" name="sampaiTanggal" id="sampaiTanggal">
+                                </div>
+                            </div>
                         </div>
-                    </div>
-                    <div class="col-6 col-md-3">
-                        <div class="form-group">
-                            <label class="form-label">s.d Tanggal</label>
-                            <input type="text" class="form-control datetimepicker-input roundedd-0 form-control-sm" name="sampaiTanggal" id="sampaiTanggal">
+                        <div class="row">
+                            <div class="col-12">
+                                <div id="divDataPelunasan"></div>
+                            </div>
                         </div>
-                    </div>
-                </div>
-                <div class="row">
-                    <div class="col-12">
-                        <div id="divDataPelunasan"></div>
                     </div>
                 </div>
             </div>
         </div>
     </div>
 </div>
+@endsection
 <script>
     $(function() {
         $( ".datetimepicker-input" ).datepicker({
