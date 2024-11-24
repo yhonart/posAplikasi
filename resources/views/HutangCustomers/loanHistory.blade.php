8fb94c931acbe39cbb4ac1a773785b82cae71ddb
yhONdev <yusuf7789@gmail.com>
2024-11-24 21:08:09 +0700

    adding loan history

--- resources/views/HutangCustomers/loanHistory.blade.php
+++ resources/views/HutangCustomers/loanHistory.blade.php
@@ -17,7 +17,10 @@
                 <td class="text-right">{{number_format($lpc->total_kredit,'0',',','.')}}</td>
                 <td class="text-right">{{number_format($lpc->debit,'0',',','.')}}</td>
                 <td>{{$lpc->customer_store}}</td>
-                <td></td>
+                <td>
+                    <a href="{{route('Cashier')}}/dataPelunasan/printPelunasan/{{$lpc->payment_number}}" class="btn btn-sm btn-success" target="_blank">Cetak Voucher</a>
+                    <button type="button" class="btn btn-sm btn-info edit-dokumen" id-number="{{$lpc->idtr_payment}}"><i class="fa-solid fa-pencil"></i></button>
+                </td>
             </tr>
         @endforeach
     </tbody>
