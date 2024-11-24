1ded9dcd05c2c0df8f3cbdfd51ba6df43c21c400
yhONdev <yusuf7789@gmail.com>
2024-11-24 21:35:50 +0700

    changes saldo summery

--- resources/views/Cashier/cashierModalDataPelunasanSummary.blade.php
+++ resources/views/Cashier/cashierModalDataPelunasanSummary.blade.php
@@ -1,8 +1,9 @@
 @php
     $grandTotalKredit = '0';
     $grandTotalBayar = '0';
+    $grandSaldoKredit = '0';
 @endphp
-<div class="card card-body table-responsive p-0" style="height:400px;">
+<div class="card card-body table-responsive p-0" style="height:500px;">
     <table class="table table-valign-middle table-hover">
         <thead class="bg-gradient-purple">
             <tr>
@@ -12,6 +13,7 @@
                 <th>Tgl. Faktur</th>
                 <th>Kredit</th>
                 <th>Dibayar</th>
+                <th>Sisa Kredit</th>
             </tr>
         </thead>
         <tbody>
@@ -21,12 +23,14 @@
                     <td>{{$dpn->customer_store}}</td>
                     <td>{{$dpn->from_payment_code}}</td>
                     <td>{{date('d-M-Y', strtotime($dpn->created_at))}}</td>
-                    <td><i class="fa-solid fa-rupiah-sign float-left"></i> <span class="float-right">{{number_format($dpn->nom_kredit,'0',',','.')}}</span></td>
+                    <td><i class="fa-solid fa-rupiah-sign float-left"></i> <span class="float-right">{{number_format($dpn->nominal,'0',',','.')}}</span></td>
                     <td><i class="fa-solid fa-rupiah-sign float-left"></i> <span class="float-right">{{number_format($dpn->nom_payed,'0',',','.')}}</span></td>
+                    <td><i class="fa-solid fa-rupiah-sign float-left"></i> <span class="float-right">{{number_format($dpn->nom_kredit,'0',',','.')}}</span></td>
                 </tr>
                 @php
-                    $grandTotalKredit += $dpn->nom_kredit; 
+                    $grandTotalKredit += $dpn->nominal; 
                     $grandTotalBayar += $dpn->nom_payed; 
+                    $grandSaldoKredit += $dpn->nom_payed; 
                 @endphp
             @endforeach
         </tbody>
@@ -38,6 +42,7 @@
                 <td></td>
                 <td class="font-weight-bold"><i class="fa-solid fa-rupiah-sign float-left"></i> <span class="float-right">{{number_format($grandTotalKredit,'0',',','.')}}</span></td>
                 <td class="font-weight-bold"><i class="fa-solid fa-rupiah-sign float-left"></i> <span class="float-right">{{number_format($grandTotalBayar,'0',',','.')}}</span></td>
+                <td class="font-weight-bold"><i class="fa-solid fa-rupiah-sign float-left"></i> <span class="float-right">{{number_format($grandSaldoKredit,'0',',','.')}}</span></td>
             </tr>
         </tbody>
     </table>
