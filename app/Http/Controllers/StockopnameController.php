525b1e9de9056ad60a7e0813595ebde2d078969d
yhONdev <yusuf7789@gmail.com>
2024-11-25 10:24:15 +0700

    add date input

--- app/Http/Controllers/StockopnameController.php
+++ app/Http/Controllers/StockopnameController.php
@@ -758,7 +758,7 @@ class StockopnameController extends Controller
                 // Insert into laporan
                 DB::table('report_inv')
                 ->insert([
-                    'date_input'=>now(),
+                    'date_input'=>$dateInput,
                     'number_code'=>$idOpname,
                     'product_id'=>$opmProduct,
                     'product_name'=>$prodName,
