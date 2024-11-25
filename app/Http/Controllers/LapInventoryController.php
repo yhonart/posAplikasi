21b0605a28fa58b99160168af1088dd571f1101c
yhONdev <yusuf7789@gmail.com>
2024-11-25 08:40:59 +0700

    adding orderby coloumn into laproan inventory

--- app/Http/Controllers/LapInventoryController.php
+++ app/Http/Controllers/LapInventoryController.php
@@ -71,6 +71,7 @@ class LapInventoryController extends Controller
             $dataReportInv = $dataReportInv->where('status_trx','4');
             $dataReportInv = $dataReportInv->whereBetween('date_input',[$fromDate, $endDate]);
             $dataReportInv = $dataReportInv->orderBy('date_input','asc');
+            $dataReportInv = $dataReportInv->orderBy('number_code','asc');
             $dataReportInv = $dataReportInv->get();
 
         $dataSaldoAwal = DB::table('report_inv');
