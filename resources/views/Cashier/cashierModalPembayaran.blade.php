6dbc908c5adbc40307e3e0f48d5982183dbcd394
yhONdev <yusuf7789@gmail.com>
2024-11-28 16:04:51 +0700

    check box

--- resources/views/Cashier/cashierModalPembayaran.blade.php
+++ resources/views/Cashier/cashierModalPembayaran.blade.php
@@ -466,7 +466,7 @@
                 else {
                     inputPembayaran(billPembayaran, typeCetak);
                 }
-                alert(replaceTotalPembayaran+"<"+replaceKredit+";"+checkBoxLunas);
+                alert(replaceTotalPembayaran+">="+replaceKredit+";"+checkBoxLunas.checked);
             }
             
             if (event.keyCode === 27) {
