2bce364eaf7d899bfc660db62c7dd76dd70f88c3
yhONdev <yusuf7789@gmail.com>
2024-11-29 15:35:52 +0700

    adding alert

--- resources/views/Cashier/cashierModalPembayaran.blade.php
+++ resources/views/Cashier/cashierModalPembayaran.blade.php
@@ -497,7 +497,7 @@
         document.addEventListener('keydown', function(event) {
             if (event.ctrlKey && event.key === 's') { // Cetak
                 event.preventDefault();
-                alert (totalHutang+">"+kreditLimit+" / "+valBelanja + " / " + valPengurangan + " / " + valHutang);
+                alert (totalHutang+">"+kreditLimit+" / "+ valBelanja + " / " + valPengurangan + " / " + valBayarTunai + " / " + valHutang);
                 let typeCetak = $("#typeCetak").val(),
                     totalPembayaran = $("#tPembayaran").val(),
                     tBelanja = $("#tBelanja").val(),
