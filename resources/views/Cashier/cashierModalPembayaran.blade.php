623d0084ad4250b8ac6510c325f2bcfde274c2c8
yhONdev <yusuf7789@gmail.com>
2024-11-29 15:14:09 +0700

    add alert kredit limit

--- resources/views/Cashier/cashierModalPembayaran.blade.php
+++ resources/views/Cashier/cashierModalPembayaran.blade.php
@@ -497,7 +497,7 @@
         document.addEventListener('keydown', function(event) {
             if (event.ctrlKey && event.key === 's') { // Cetak
                 event.preventDefault();
-                alert (totalHutang);
+                alert (totalHutang+">"+kreditLimit);
                 let typeCetak = $("#typeCetak").val(),
                     totalPembayaran = $("#tPembayaran").val(),
                     tBelanja = $("#tBelanja").val(),
