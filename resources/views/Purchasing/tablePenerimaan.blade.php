9c1e1a62248f215d35d54c6a666799c02dfd5726
yhONdev <yusuf7789@gmail.com>
2024-11-30 18:03:23 +0700

    adding dom

--- resources/views/Purchasing/tablePenerimaan.blade.php
+++ resources/views/Purchasing/tablePenerimaan.blade.php
@@ -72,8 +72,9 @@ $statusDokumen = array(
             "responsive": true, 
             "lengthChange": false, 
             "autoWidth": false,
+            "dom": 'Bfrtip',
             "buttons": ["copy", "csv", "excel", "pdf", "print", "colvis"]
-        })
+        }).buttons().container().appendTo('#example1_wrapper .col-md-6:eq(0)');
     });
     $(document).ready(function(){
         $('.BTN-EDIT').on('click', function (e) {
