57451e518df57a8f2c0b6996b46f986d17a205b5
yhONdev <yusuf7789@gmail.com>
2024-11-30 17:50:07 +0700

    changes datatables

--- resources/views/Purchasing/tablePenerimaan.blade.php
+++ resources/views/Purchasing/tablePenerimaan.blade.php
@@ -68,20 +68,11 @@ $statusDokumen = array(
 
 <script>
     $(function(){
-        $('#tableListPembelian').DataTable({
-            "paging": true,
-            "lengthChange": true,
-            "searching": true,
-            "ordering": true,
-            "info": true,
-            "autoWidth": false,
-            "responsive": true,
-            layout: {
-                topStart: {
-                    buttons: ['copy', 'csv', 'excel', 'pdf', 'print']
-                }
-            }
-        });
+        
+        $("#tableListPembelian").DataTable({
+            "responsive": true, "lengthChange": false, "autoWidth": false,
+            "buttons": ["copy", "csv", "excel", "pdf", "print", "colvis"]
+        }).buttons().container().appendTo('#example1_wrapper .col-md-6:eq(0)');
     });
     $(document).ready(function(){
         $('.BTN-EDIT').on('click', function (e) {
