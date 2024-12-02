efcb7c94c215d51ddb38a16c7fb32f936e170db1
yhONdev <yusuf7789@gmail.com>
2024-12-02 11:14:13 +0700

    remove method

--- resources/views/Report/cashierRecapExcel.blade.php
+++ resources/views/Report/cashierRecapExcel.blade.php
@@ -1,57 +1,8 @@
 <?php 
-    header("Content-type: application/vnd-ms-excel");
-    header("Content-Disposition: attachment; filename=ReportHarian.xls");
+    // header("Content-type: application/vnd-ms-excel");
+    // header("Content-Disposition: attachment; filename=ReportHarian.xls");
     $no = '1';
 ?>
-<!-- <style>
-    .styled-table {
-        border-collapse: collapse;
-        margin: 25px 0;
-        font-size: 12;
-        font-family: sans-serif;
-        min-width: 400px;
-        box-shadow: 0 0 20px rgba(0, 0, 0, 0.15);
-        border: 1px solid #dee2e6;
-    }.styled-table-2 tbody tr th,
-    .styled-table thead tr {
-        background-color: #17a2b8;
-        color: #ffffff;
-        text-align: left;
-    }
-    .styled-table th,
-    .styled-table td {
-        padding: 6px 7px;
-        border: 1px solid #dee2e6;
-    }
-    .styled-table tbody tr.active-row {
-        font-weight: bold;
-        color: #009879;
-    }
-    .styled-table tbody{
-        vertical-align: top;
-        border: 1px solid #dee2e6;
-    }
-    .styled-table .date_check{
-        width: 100%;
-    }
-    .judul{
-        text-align: center;
-        font-size: 1em;
-        font-family: sans-serif;
-    }
-    .judul p{
-        margin: 0.1em;
-    }
-    .span {
-        font-size :8;
-    }
-    .total {
-        font-size : 7;
-    }
-    .grand-total {
-        font-size : 8;
-    }
-</style> -->
 <?php
     $sumHrgSatuan = '0';
     $sumJumlah = '0';
@@ -110,17 +61,7 @@
                 <td>{{$ptrx->disc}}</td>
                 <td>{{$ptrx->t_price}}</td>
                 <td>
-                    @foreach($countPerTrx as $cPtx)
-                        @if($cPtx->core_id_trx == $ptrx->billing_number)
-                            @if($cPtx->countTrx == '1')
-                                @foreach($paymentMethod as $payM)
-                                    @if($payM->core_id_trx == $ptrx->billing_number)
-                                        {{$payM->method_name}}
-                                    @endif
-                                @endforeach
-                            @endif
-                        @endif
-                    @endforeach
+                    
                 </td>
                 <td></td>
                 <td></td>
