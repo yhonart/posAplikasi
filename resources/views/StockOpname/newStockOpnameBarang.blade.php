d46b63dd9651252aa25bad67475537f9900fd388
yhONdev <yusuf7789@gmail.com>
2024-12-04 16:25:47 +0700

    changes button

--- resources/views/StockOpname/newStockOpnameBarang.blade.php
+++ resources/views/StockOpname/newStockOpnameBarang.blade.php
@@ -11,9 +11,9 @@
             <div class="card-body table-responsive">
                 <div class="row mb-2">
                     <div class="col-12">
-                        <button class="btn btn-sm bg-light font-weight-bold border-0 pl-2 text-primary" id="displayDocument"><i class="fa-solid fa-file-pen"></i> Dokumen Opname Barang</button>
-                        <button class="btn btn-sm bg-light font-weight-bold border-0 pl-2 text-success" id="saveDocument"><i class="fa-regular fa-floppy-disk"></i> Simpan Transaksi</button>
-                        <button class="btn btn-sm bg-light font-weight-bold border-0 pl-2 text-danger" id="cencelDocument"><i class="fa-solid fa-xmark"></i> Batalkan Transaksi</button>
+                        <button class="btn btn-sm btn-default font-weight-bold border-0 pl-2" id="displayDocument"><i class="fa-solid fa-file-pen"></i> <span class="text-primary">Dokumen Opname Barang</span></button>
+                        <button class="btn btn-sm btn-default font-weight-bold border-0 pl-2" id="saveDocument"><i class="fa-regular fa-floppy-disk"></i> <span class="text-success">Simpan Transaksi</span></button>
+                        <button class="btn btn-sm btn-default font-weight-bold border-0 pl-2" id="cencelDocument"><i class="fa-solid fa-xmark"></i> <span class="text-danger">Batalkan Transaksi</span></button>
                     </div>
                 </div>
                 <div class="row mb-2">
