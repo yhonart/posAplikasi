97b519feae58ea1ee4db26005e840a1e275230f2
yhONdev <yusuf7789@gmail.com>
2024-12-09 11:22:42 +0700

    command update stock

--- app/Http/Controllers/MutasibarangController.php
+++ app/Http/Controllers/MutasibarangController.php
@@ -602,17 +602,17 @@ class MutasibarangController extends Controller
             $this->TempInventoryController->reportBarangMasuk ($productID, $invID, $satuan, $toLoc, $takenStock, $description, $idParam, $updateBy);
         }
         
-        DB::table('inv_moving')
-            ->where('number',$idParam)
-            ->update([
-                'status'=>'4'    
-            ]);
+        // DB::table('inv_moving')
+        //     ->where('number',$idParam)
+        //     ->update([
+        //         'status'=>'4'    
+        //     ]);
         
-        DB::table('inv_moving_list')
-            ->where('mutasi_code',$idParam)
-            ->update([
-                'status'=>'4'    
-            ]);        
+        // DB::table('inv_moving_list')
+        //     ->where('mutasi_code',$idParam)
+        //     ->update([
+        //         'status'=>'4'    
+        //     ]);        
    }
    
    public function editMutasi ($idparam){
