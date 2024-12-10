a1f0f62ac8e09de64cf1d7d80188e9e72b52e508
yhONdev <yusuf7789@gmail.com>
2024-12-10 22:45:29 +0700

    changes table MOU

--- resources/views/AssetManagement/MasterData/MoUTableData.blade.php
+++ resources/views/AssetManagement/MasterData/MoUTableData.blade.php
@@ -1,8 +1,8 @@
 @php
     $no = 1;
 @endphp
-<table class="table table-striped table-valign-middle table-sm" id="tableDataMou">
-    <thead class="bg-purple text-center">
+<table class="table table-hover table-valign-middle table-sm" id="tableDataMou">
+    <thead class="bg-gray">
         <tr>
             <th>No.</th>
             <th>Inisial</th>
@@ -18,7 +18,7 @@
                 <td>{{$mou->unit_note}}</td>
                 <td class="text-right">
                     <a class="btn btn-info btn-sm BTN-OPEN-MODAL-GLOBAL-LG" href="{{route('MoU')}}/tableMoU/EditMoU/{{$mou->idm_unit}}"><i class="fa-solid fa-pencil"></i> Edit Unit</a>                                    
-                    <a class="btn btn-info btn-sm BTN-OPEN-MODAL-GLOBAL-LG" href="{{route('MoU')}}/tableMoU/DeleteMoU/{{$mou->idm_unit}}"><i class="fa-solid fa-trash-can"></i> Delete Permanently</a>
+                    <a class="btn btn-info btn-sm DELETE-BTN" data-id="{{$mou->idm_unit}}" href="{{route('MoU')}}/tableMoU/DeleteMoU/{{$mou->idm_unit}}"><i class="fa-solid fa-trash-can"></i> Delete Permanently</a>
                 </td>
             </tr>
         @endforeach
