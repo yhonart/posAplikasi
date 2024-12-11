5d9da00d0e7f1f6fa1096299fab9c393439ddae6
yhONdev <yusuf7789@gmail.com>
2024-12-11 20:52:41 +0700

    remove other y

--- resources/views/Dashboard/DashboardGarphPenjualan.blade.php
+++ resources/views/Dashboard/DashboardGarphPenjualan.blade.php
@@ -60,26 +60,7 @@
                     {{$ySeriesData2->totalPayment}},
                 @endforeach
             ]
-        }, {
-            name: 'Sales & Distribution',
-            data: [
-                11744, 30000, 16005, 19771, 20185, 24377,
-                32147, 30912, 29243, 29213, 25663, 28978, 30618
-            ]
-        }, {
-            name: 'Operations & Maintenance',
-            data: [
-                null, null, null, null, null, null, null,
-                null, 11164, 11218, 10077, 12530, 16585
-            ]
-        }, {
-            name: 'Other',
-            data: [
-                21908, 5548, 8105, 11248, 8989, 11816, 18274,
-                17300, 13053, 11906, 10073, 11471, 11648
-            ]
-        }],
-
+        },
         responsive: {
             rules: [{
                 condition: {
