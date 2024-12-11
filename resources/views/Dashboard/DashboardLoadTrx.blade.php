c3e299ba8c854117d5d9095c5afa1abfbb4ac34c
yhONdev <yusuf7789@gmail.com>
2024-12-11 20:18:34 +0700

    remove graph card

--- resources/views/Dashboard/DashboardLoadTrx.blade.php
+++ resources/views/Dashboard/DashboardLoadTrx.blade.php
@@ -91,30 +91,6 @@
         </div>
     </div>
 </div>
-
-<div class="row">
-    <div class="col-12 col-md-6">
-        <div class="card">
-          <div class="card-header">
-            <h3 class="card-title">
-              <i class="fas fa-chart-pie mr-1"></i>
-              Garp Penjualan
-            </h3>
-          </div>
-          <div class="card-body">
-            <div id="container"></div>
-          </div>
-        </div>
-    </div>
-    <div class="col-12 col-md-6">
-        <div class="row">
-            <div class="col-12">
-                
-            </div>
-        </div>
-    </div>
-</div>
-
 <script>
     $(function() {
         let selectYear1 = $("#pilihTahun").val();
