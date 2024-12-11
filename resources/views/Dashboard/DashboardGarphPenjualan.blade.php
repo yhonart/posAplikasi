c3a746b69be990da42302bb5efe0658457b5a7a5
yhONdev <yusuf7789@gmail.com>
2024-12-11 20:42:08 +0700

    changes plot

--- resources/views/Dashboard/DashboardGarphPenjualan.blade.php
+++ resources/views/Dashboard/DashboardGarphPenjualan.blade.php
@@ -1,48 +1,7 @@
 <div id="container"></div>
 
 <script>
-    Highcharts.chart('textcontrol', {
-        chart: {
-            type: 'column'
-        },
-        title: {
-            text: 'Data Penjualan Per Bulan',
-            align: 'left'
-        },
-        xAxis: {
-            categories: [   
-                @foreach($penjualanVSPembelian as $xAxisDate)
-                    '{{$xAxisDate->displayPeriode}}',
-                @endforeach
-            ],
-            crosshair: true,
-            accessibility: {
-                description: 'Countries'
-            }
-        },
-        yAxis: {
-            min: 0,
-            title: {
-                text: 'Total Pendapatan'
-            }
-        },
-        plotOptions: {
-            column: {
-                pointPadding: 0.2,
-                borderWidth: 0
-            }
-        },
-        series: [
-            {
-                name: 'Total',
-                data: [
-                    @foreach($penjualanVSPembelian as $ySeriesData1)
-                        {{$ySeriesData1->totalPayment}},
-                    @endforeach
-                ]
-            }
-        ]
-    });
+    
 
     Highcharts.chart('container', {
         title: {
