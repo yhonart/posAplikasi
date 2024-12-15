c30b7174b5ae4edc6c53b705c8d1343f971a390d
yhONdev <yusuf7789@gmail.com>
2024-12-15 21:23:20 +0700

    changes ordering date_input

--- app/Http/Controllers/LapInventoryController.php
+++ app/Http/Controllers/LapInventoryController.php
@@ -70,7 +70,7 @@ class LapInventoryController extends Controller
             }
             $dataReportInv = $dataReportInv->where('status_trx','4');
             $dataReportInv = $dataReportInv->whereBetween('date_input',[$fromDate, $endDate]);
-            $dataReportInv = $dataReportInv->orderBy('idr_inv','asc');
+            $dataReportInv = $dataReportInv->orderBy('date_input','asc');
             $dataReportInv = $dataReportInv->get();
 
         $dataSaldoAwal = DB::table('report_inv');
@@ -82,7 +82,7 @@ class LapInventoryController extends Controller
             }
             $dataSaldoAwal = $dataSaldoAwal->where('status_trx','4');
             $dataSaldoAwal = $dataSaldoAwal->whereBetween('date_input',[$fromDate, $endDate]);
-            $dataSaldoAwal = $dataSaldoAwal->orderBy('idr_inv','asc');
+            $dataSaldoAwal = $dataSaldoAwal->orderBy('date_input','asc');
             $dataSaldoAwal = $dataSaldoAwal->first();
 
         $mProduct = DB::table('m_product')
