3a8d8701fa7bd34fd523283e2d1f4998cbfe98f4
yhONdev <yusuf7789@gmail.com>
2024-12-19 20:33:43 +0700

    change color bg-gray-dark

--- resources/views/HutangCustomers/saldoKreditCustomer.blade.php
+++ resources/views/HutangCustomers/saldoKreditCustomer.blade.php
@@ -2,7 +2,7 @@
     $no = '1';
 ?>
 <table class="table table-sm tabl-valign-middle table-hover" id="tableKreditCustomer">
-    <thead class="bg-lightblue">
+    <thead class="bg-gray-dark">
         <tr>
             <th>No.</th>
             <th>Nama Pelanggan</th>
