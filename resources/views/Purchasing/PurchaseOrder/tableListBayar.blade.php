533713b6f4d7d728c99f671223a2d7dc5395d389
yhONdev <yusuf7789@gmail.com>
2024-12-24 06:22:27 +0700

    change table pembayaran

--- resources/views/Purchasing/PurchaseOrder/tableListBayar.blade.php
+++ resources/views/Purchasing/PurchaseOrder/tableListBayar.blade.php
@@ -1,42 +1,40 @@
-<div class="card card-body table-responsive p-0">
-    <table class="table table-sm table-align-middle text-nowrap table-hover ">
-        <thead class="bg-gray-dark">
+<table class="table table-sm table-align-middle text-nowrap table-hover ">
+    <thead class="bg-gray-dark">
+        <tr>
+            <th>Nomor</th>
+            <th>Tgl. Penerimaan</th>
+            <th>Jatuh Tempo</th>
+            <th>Supplier</th>
+            <th>Nominal</th>
+            <th>Bayar</th>
+            <th>Keterangan</th>
+        </tr>
+    </thead>
+    <tbody>
+        @foreach($tbPurchase as $tPayment)
             <tr>
-                <th>Nomor</th>
-                <th>Tgl. Penerimaan</th>
-                <th>Jatuh Tempo</th>
-                <th>Supplier</th>
-                <th>Nominal</th>
-                <th>Bayar</th>
-                <th>Keterangan</th>
+                <td>{{$tPayment->purchase_number}}</td>
+                <td>{{date("d/M/Y", strtotime($tPayment->delivery_date))}}</td>
+                <td>
+                    <?php
+                        $dodate = date("d/M/Y", strtotime("+".$tPayment->tempo."day",strtotime($tPayment->delivery_date)));
+                    ?>
+                    {{$dodate}}
+                </td>
+                <td>{{$tPayment->store_name}}</td>
+                <td class="text-right">
+                    <input type="text" class="form-control form-control-sm form-control-border price-tag" name="kredit" id="kredit" value="{{$tPayment->sub_total}}" readonly>
+                </td>
+                <td>
+                    <input type="text" placeholder="Input nominal disini" class="form-control form-control-sm form-control-border price-tag" name="pay" id="pay" autocomplete="off" onchange="saveChangePembayaran(this,'purchase_kredit_payment','kredit_pay','{{$tPayment->purchase_number}}','nomor')">    
+                </td>
+                <td>
+                    
+                </td>
             </tr>
-        </thead>
-        <tbody>
-            @foreach($tbPurchase as $tPayment)
-                <tr>
-                    <td>{{$tPayment->purchase_number}}</td>
-                    <td>{{date("d/M/Y", strtotime($tPayment->delivery_date))}}</td>
-                    <td>
-                        <?php
-                            $dodate = date("d/M/Y", strtotime("+".$tPayment->tempo."day",strtotime($tPayment->delivery_date)));
-                        ?>
-                        {{$dodate}}
-                    </td>
-                    <td>{{$tPayment->store_name}}</td>
-                    <td class="text-right">
-                        <input type="text" class="form-control form-control-sm form-control-border price-tag" name="kredit" id="kredit" value="{{$tPayment->sub_total}}" readonly>
-                    </td>
-                    <td>
-                        <input type="text" placeholder="Input nominal disini" class="form-control form-control-sm form-control-border price-tag" name="pay" id="pay" autocomplete="off" onchange="saveChangePembayaran(this,'purchase_kredit_payment','kredit_pay','{{$tPayment->purchase_number}}','nomor')">    
-                    </td>
-                    <td>
-                        
-                    </td>
-                </tr>
-            @endforeach
-        </tbody>
-    </table>
-</div>
+        @endforeach
+    </tbody>
+</table>
 <script>
         $('.price-tag').mask('000.000.000', {reverse: true});
         var el_modal_all = $('.MODAL-GLOBAL'),
