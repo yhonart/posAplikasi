d36002814f4a347c92f2f90ec1a016a31cfb2b98
yhONdev <yusuf7789@gmail.com>
2024-12-26 21:50:22 +0700

    change display pembayaran purchasing

--- resources/views/Purchasing/PurchaseOrder/modalBayar.blade.php
+++ resources/views/Purchasing/PurchaseOrder/modalBayar.blade.php
@@ -4,32 +4,28 @@
             <form id="formPayMethod">
                 <input type="text" name="idKredit" value="{{$tbPayment->idp_pay}}">
                 <div class="form-group row">
-                    <label class="col-md-3">AP Number</label>
+                    <label class="col-md-2">AP Number</label>
                     <div class="col-md-3">
                         <input type="text" class="form-control form-control-sm" name="apNumber" id="apNumber" value="{{$tbPayment->nomor}}" readonly>
                     </div>
-                    <label class="col-3">Purchase Number</label>
-                    <div class="col-3">
+                    <label class="col-md-2">Purchase Number</label>
+                    <div class="col-md-2">
                         <input type="text" class="form-control form-control-sm" name="purchaseNumber" id="purchaseNumber" value="{{$datPayment->number_dok}}" readonly>
                     </div>
                 </div>
                 <div class="form-group row">
-                </div>
-                <div class="form-group row">
-                    <label class="col-md-3">Nominal Kredit</label>
+                    <label class="col-md-2">Nominal Kredit</label>
                     <div class="col-md-3">
                         <input type="text" class="form-control form-control-sm price-tag" name="nominalKredit" id="nominalKredit" value="{{$datPayment->kredit}}" readonly>
                     </div>
                 </div>
                 <div class="form-group row">
-                    <label class="col-4">Nominal Bayar</label>
-                    <div class="col-6">
+                    <label class="col-md-2">Nominal Bayar</label>
+                    <div class="col-md-3">
                         <input type="text" class="form-control form-control-sm price-tag" name="nominal" id="nominal" value="{{$tbPayment->kredit_pay}}">
                     </div>
-                </div>
-                <div class="form-group row">
-                    <label class="col-4">Selisih</label>
-                    <div class="col-6">
+                    <label class="col-2">Selisih</label>
+                    <div class="col-3">
                         <?php
                             $selisih = $datPayment->kredit - $tbPayment->kredit_pay;
                         ?>
@@ -37,17 +33,15 @@
                     </div>
                 </div>
                 <div class="form-group row">
-                    <label class="col-4">Metode Pembayaran</label>
-                    <div class="col-6">
+                    <label class="col-md-2">Metode Pembayaran</label>
+                    <div class="col-md-3">
                         <select class="form-control form-control-sm" name="method" id="method">
                             <option value="TUNAI">TUNAI</option>
                             <option value="TRANSFER">TRANSFER</option>
                         </select>
                     </div>
-                </div>
-                <div class="form-group row">
-                    <label class="col-4">Akun Pembayaran <small class="text-muted">[Optional]</small></label>
-                    <div class="col-6">
+                    <label class="col-md-2">Akun Pembayaran <small class="text-muted">[Optional]</small></label>
+                    <div class="col-md-3">
                         <select class="form-control form-control-sm" name="account" id="account">
                             <option value="">....</option>
                             <option value="BANK">BANK</option>
@@ -57,22 +51,20 @@
                     </div>
                 </div>
                 <div class="form-group row">
-                    <label class="col-4">Nama Akun Bank <small class="text-muted">[Optional]</small></label>
-                    <div class="col-6">
+                    <label class="col-md-2">Nama Akun Bank <small class="text-muted">[Optional]</small></label>
+                    <div class="col-md-3">
                         <input type="text" class="form-control form-control-sm" name="accountName" id="accountName" placeholder="Jika menggunnakan selain tunai">
                     </div>
-                </div>
-                <div class="form-group row">
-                    <label class="col-4">Nomor Akun Bank <small class="text-muted">[Optional]</small></label>
-                    <div class="col-6">
+                    <label class="col-md-4">Nomor Akun Bank <small class="text-muted">[Optional]</small></label>
+                    <div class="col-md-6">
                         <input type="text" class="form-control form-control-sm" name="accountNumber" id="accountNumber" placeholder="Jika menggunnakan selain tunai">
                     </div>
                 </div>
                 <div class="form-group row">
-                    <div class="col-md-3">
+                    <div class="col-md-2">
                         <button type="submit" class="btn btn-success btn-sm elevation-2 font-weight-bold btn-block" id="submitPembayaran"><i class="fa-solid fa-receipt"></i> Bayar</button>
                     </div>
-                    <div class="col-md-3">
+                    <div class="col-md-2">
                         <button type="submit" class="btn btn-danger btn-sm elevation-2 font-weight-bold btn-block" id="batalPembayaran"><i class="fa-solid fa-circle-xmark"></i> Batal</button>
                     </div>
                 </div>
