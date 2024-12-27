28340af19047e7fa3868a7ddb3844cd5641fd233
yhONdev <yusuf7789@gmail.com>
2024-12-27 15:00:11 +0700

    changes button download

--- resources/views/AssetManagement/MasterData/CustomersIndex.blade.php
+++ resources/views/AssetManagement/MasterData/CustomersIndex.blade.php
@@ -14,7 +14,10 @@
     <div class="container-fluid">
         <div class="row mt-2">
             <div class="col-12 col-md-3">
-                <button type="button" class="btn btn-primary  font-weight-bold BTN-OPEN-MODAL-GLOBAL-LG btn-block mb-2" href="{{route('Customers')}}/AddCustomers" ><i class="fa-solid fa-address-book"></i> Tambah Pelanggan</button>
+                <div class="btn-group btn-block mb-2">
+                    <button type="button" class="btn btn-primary font-weight-bold BTN-OPEN-MODAL-GLOBAL-LG" href="{{route('Customers')}}/AddCustomers" ><i class="fa-solid fa-address-book"></i> Tambah</button>
+                    <a href="{{route('Customers')}}/downloadAllCustomer" class="btn btn-success font-weight-bold" target="_blank"><i class="fa-solid fa-file-excel"></i> Download</a>
+                </div>
                 <input type="text" name="searchCustomer" id="searchCustomer" class="form-control mb-1 " placeholder="Cari nama pelanggan" autofocus>
                 <div class="card card-body p-0 table-responsive " style="height:700px;">
                     @include('Global.global_spinner')
@@ -22,9 +25,6 @@
                 </div>
             </div>
             <div class="col-12 col-md-9">
-                <div id="displayDownload">
-                    <a href="{{route('Customers')}}/downloadAllCustomer" class="btn btn-success btn-sm" target="_blank"><i class="fa-solid fa-file-excel"></i> Download Semua Customer</a>
-                </div>
                 <div id="displayEditCos"></div>
             </div>
         </div>
