db8f015234ee64e29e60ea4d16c41b3ee2a4e8e4
yhONdev <yusuf7789@gmail.com>
2025-01-04 22:13:57 +0700

    add card into detail item

--- resources/views/ReturnItem/displayPurchaseDetailItem.blade.php
+++ resources/views/ReturnItem/displayPurchaseDetailItem.blade.php
@@ -1,28 +1,30 @@
 <div class="row">
     <div class="col-md-12">
-        <table class="table table-sm table-valign-middle table-hover">
-            <thead>
-                <tr>
-                    <th>Nama Barang</th>
-                    <th>Satuan</th>
-                    <th>Qty</th>
-                    <th class="text-right">Hrg. Satuan</th>
-                    <th class="text-right">Total</th>
-                    <th>Lokasi</th>
-                </tr>
-            </thead>
-            <tbody>
-                @foreach($viewPurchaseOrder as $item)
+        <div class="card card-body">
+            <table class="table table-sm table-valign-middle table-hover">
+                <thead>
                     <tr>
-                        <td>{{$item->product_name}}</td>
-                        <td>{{$item->satuan}}</td>
-                        <td>{{$item->qty}}</td>
-                        <td class="text-right">{{number_format($item->unit_price,'0',',','.')}}</td>
-                        <td class="text-right">{{number_format($item->total_price,'0',',','.')}}</td>
-                        <td>{{$item->site_name}}</td>
+                        <th>Nama Barang</th>
+                        <th>Satuan</th>
+                        <th>Qty</th>
+                        <th class="text-right">Hrg. Satuan</th>
+                        <th class="text-right">Total</th>
+                        <th>Lokasi</th>
                     </tr>
-                @endforeach
-            </tbody>
-        </table>
+                </thead>
+                <tbody>
+                    @foreach($viewPurchaseOrder as $item)
+                        <tr>
+                            <td>{{$item->product_name}}</td>
+                            <td>{{$item->satuan}}</td>
+                            <td>{{$item->qty}}</td>
+                            <td class="text-right">{{number_format($item->unit_price,'0',',','.')}}</td>
+                            <td class="text-right">{{number_format($item->total_price,'0',',','.')}}</td>
+                            <td>{{$item->site_name}}</td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
     </div>
 </div>
