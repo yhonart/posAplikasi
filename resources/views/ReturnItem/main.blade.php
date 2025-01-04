2507d6f12858ac360d44af72bb9cb13316dbc184
yhONdev <yusuf7789@gmail.com>
2025-01-04 22:25:11 +0700

    fix bug link

--- resources/views/ReturnItem/main.blade.php
+++ resources/views/ReturnItem/main.blade.php
@@ -10,7 +10,7 @@
                 <div class="col-sm-6">
                     <ol class="breadcrumb float-sm-right">
                       <li class="breadcrumb-item"><a href="#"><i class="fa-solid fa-house"></i></a></li>
-                      <li class="breadcrumb-item"><a href="#">Pembelian</li>
+                      <li class="breadcrumb-item">Pembelian</li>
                       <li class="breadcrumb-item">Retur Barang</li>
                     </ol>
                  </div>          
