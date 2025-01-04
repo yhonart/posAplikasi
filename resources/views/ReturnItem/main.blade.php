1f3f9aa664ca2678808f6c5a9bd6dd94862f7c0f
yhONdev <yusuf7789@gmail.com>
2025-01-04 22:16:31 +0700

    change main display retur barang

--- resources/views/ReturnItem/main.blade.php
+++ resources/views/ReturnItem/main.blade.php
@@ -5,11 +5,12 @@
         <div class="container-fluid">
             <div class="row mb-2">
                 <div class="col-sm-6">          
-                    <h1 class="m-0">Transaksi <small class="text-info">Retur Pembelian</small></h1>
+                    <h1 class="m-0">Retur Barang</h1>
                 </div> 
                 <div class="col-sm-6">
                     <ol class="breadcrumb float-sm-right">
                       <li class="breadcrumb-item"><a href="#"><i class="fa-solid fa-house"></i></a></li>
+                      <li class="breadcrumb-item"><a href="#">Pembelian</li>
                       <li class="breadcrumb-item">Retur Barang</li>
                     </ol>
                  </div>          
