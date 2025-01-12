9743aadc0737c52ccdf55114a719e639b59a3e29
yhONdev <yusuf7789@gmail.com>
2025-01-12 15:24:18 +0700

    change start week to monday

--- resources/views/TrxKasBesar/laporanKasBesar.blade.php
+++ resources/views/TrxKasBesar/laporanKasBesar.blade.php
@@ -45,7 +45,14 @@
             todayHighlight: true,
         });
 
-        var curr = new Date;
+        function getMonday(d) {
+            d = new Date(d);
+            var day = d.getDay(),
+                diff = d.getDate() - day + (day == 0 ? -6 : 1); // adjust when day is sunday
+            return new Date(d.setDate(diff));
+        }
+
+        var curr = getMonday(new Date());
         var first = curr.getDate() - curr.getDay();
         var last = first + 6;
         var firstDate = new Date(curr.setDate(first));
