3dcb954bea7026059ef1ea2d070d718ccd9730ee
yhONdev <yusuf7789@gmail.com>
2025-01-12 14:07:43 +0700

    change route kas besar

--- routes/section/trxKasBesar.php
+++ routes/section/trxKasBesar.php
@@ -1,3 +1,7 @@
+<?php
+use Illuminate\Support\Facades\Route;
 Route::get('kasBesar/laporanKasBesar', [App\Http\Controllers\TrxKasBesarController::class, 'laporanKasBesar']);
 Route::get('kasBesar/tableLaporan/{kasir}/{fromDate}/{endDate}', [App\Http\Controllers\TrxKasBesarController::class, 'tableLaporan']);
 Route::get('kasBesar/dashboardKasBesar', [App\Http\Controllers\TrxKasBesarController::class, 'dashboardKasBesar']);
+
+?>
