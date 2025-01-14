32a5b5a6cceb188f6b05fd7306c215f0b9bf53e3
yhONdev <yusuf7789@gmail.com>
2025-01-14 23:11:05 +0700

    cgange profuk satuan

--- resources/views/ReturnItem/displaySelectSatuan.blade.php
+++ resources/views/ReturnItem/displaySelectSatuan.blade.php
@@ -1,5 +1,5 @@
 <select class="form-control form-control-sm " name="satuan" id="satuan">
     @foreach($satuanItem as $itemSatuan)
-        <option value="{{$itemSatuan->size}}">{{$itemSatuan->satuan}}</option>
+        <option value="{{$itemSatuan->product_size}}">{{$itemSatuan->satuan}}</option>
     @endforeach
 </select>
