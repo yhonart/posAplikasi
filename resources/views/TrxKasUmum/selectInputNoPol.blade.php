cd968d99007e627685559d94430ac9633e4a2fd2
yhONdev <yusuf7789@gmail.com>
2025-01-15 16:28:11 +0700

    change where empty nopol

--- resources/views/TrxKasUmum/selectInputNoPol.blade.php
+++ resources/views/TrxKasUmum/selectInputNoPol.blade.php
@@ -1,5 +1,5 @@
 @if(!empty($selectNopol))
     <input type="text" name="nopol" id="nopol" class="form-control form-control-sm" value="{{$selectNopol->no_utility}}">
 @else
-    <input type="text" name="nopol" id="nopol" class="form-control form-control-sm" value="0">
+    <input type="text" name="nopol" id="nopol" class="form-control form-control-sm" value="">
 @endif
