3741c0f8c53dc860d01ba438a3db09113040698e
yhONdev <yusuf7789@gmail.com>
2025-01-16 21:08:33 +0700

    add style height

--- resources/views/TrxKasKecil/laporanKasKecilTable.blade.php
+++ resources/views/TrxKasKecil/laporanKasKecilTable.blade.php
@@ -5,7 +5,7 @@ $kredit = 0;
 ?>
 <a href="{{route('kasKecil')}}/cetakKasKecil/0/{{$fromDate}}/{{$endDate}}" class="btn btn-success btn-sm"><i class="fa-solid fa-file-excel"></i> Download Excel</a>
 <hr>
-<div class="table-responsive">
+<div class="table-responsive" style="height: 700px;">
     <table class="table table-sm table-hover table-valign-mmiddle text-nowrap table-bordered" id="tableKasKecil">
         <thead class="bg-gray-dark">
             <tr>
