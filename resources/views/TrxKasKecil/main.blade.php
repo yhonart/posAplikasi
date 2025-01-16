d1ef057846a844b4f0db1c721455dc1bb4183551
yhONdev <yusuf7789@gmail.com>
2025-01-16 11:26:44 +0700

    change brn-group

--- resources/views/TrxKasKecil/main.blade.php
+++ resources/views/TrxKasKecil/main.blade.php
@@ -20,9 +20,11 @@
     <div class="container-fluid">
         <div class="row mb-2">
             <div class="col-md-12">
-                <button type="button" class="btn btn-info CLIK-LAP font-weight-bold" data-display="laporanKasKecil"><i class="fa-solid fa-file-invoice-dollar"></i> Laporan</button>
-                <button type="button" class="btn btn-info CLIK-LAP font-weight-bold" data-display="dashboardKasKecil"><i class="fa-solid fa-chart-line"></i> Dashboard</button>
-                <button type="button" class="btn btn-info CLIK-LAP font-weight-bold" data-display="addModalKas"><i class="fa-solid fa-plus"></i> Tambah Saldo</button>
+                <div class="btn-group">
+                    <button type="button" class="btn btn-default CLIK-LAP font-weight-bold" data-display="laporanKasKecil"><i class="fa-solid fa-file-invoice-dollar"></i> Laporan</button>
+                    <button type="button" class="btn btn-default CLIK-LAP font-weight-bold" data-display="dashboardKasKecil"><i class="fa-solid fa-chart-line"></i> Dashboard</button>
+                    <button type="button" class="btn btn-default CLIK-LAP font-weight-bold" data-display="addModalKas"><i class="fa-solid fa-plus"></i> Tambah Saldo</button>
+                </div>
             </div>
         </div>
         <div id="displayLap"></div>
