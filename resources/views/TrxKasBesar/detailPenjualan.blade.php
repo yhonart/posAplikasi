893c9172978266e55ec65520cbfea041b3a93b4a
yhONdev <yusuf7789@gmail.com>
2025-01-18 08:01:04 +0700

    change date format

--- resources/views/TrxKasBesar/detailPenjualan.blade.php
+++ resources/views/TrxKasBesar/detailPenjualan.blade.php
@@ -4,7 +4,7 @@
     <div class="container-fluid">
         <div class="row mb-2">
             <div class="col-sm-6">
-                <h1 class="m-0">Penjualan {{$akun}} <small>Date:{{date_format('d-M-Y')}}</small></h1>
+                <h1 class="m-0">Penjualan {{$akun}} <small>Date:{{date('d-M-Y', strtotime($date))}}</small></h1>
             </div><!-- /.col -->
             <div class="col-sm-6">
                 <ol class="breadcrumb float-sm-right">
