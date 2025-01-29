0ba88a162d1ba83c57bcfc2a0a89429843cd93d4
yhONdev <yusuf7789@gmail.com>
2025-01-29 13:40:22 +0700

    change display

--- resources/views/Cashier/cashierModalDataPelunasanList.blade.php
+++ resources/views/Cashier/cashierModalDataPelunasanList.blade.php
@@ -66,7 +66,6 @@
                             {{number_format($dP->nom_payed,'0',',','.')}}
                         @elseif($dP->status == '1')
                             @foreach($getLastRecord as $glr)
-                                {{$glr->total_payment}}
                                 @if($glr->trx_code == $dP->from_payment_code)
                                     <input type="text" name="bayarPiutang" id="bayarPiutang{{$dP->idtr_kredit}}" class="form-control form-control-sm form-control-border editInput nominal-bayar price-tag" autocomplete="off" onchange="saveChangeRecord(this,'tr_kredit_record','total_payment','{{$glr->idtr_kredit_record}}','idtr_kredit_record','1')" value="{{$glr->total_payment}}">
                                 @endif
