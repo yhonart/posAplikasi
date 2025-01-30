fc8f608b5863e0f1945d2e62a3e1fef2a0dd03b0
yhONdev <yusuf7789@gmail.com>
2025-01-30 22:51:23 +0700

    change voucher code

--- resources/views/ReturnItem/displayPurchaseReturnHistory.blade.php
+++ resources/views/ReturnItem/displayPurchaseReturnHistory.blade.php
@@ -8,13 +8,13 @@ $arrStatus = array(
 );
 ?>
 <div class="card card-body">
-    <table class="table table-sm table-valign-middle table-hover">
+    <table class="table table-valign-middle table-hover">
         <thead class="bg-gray-dark">
             <tr>
                 <th>Supplier</th>
                 <th>Purchase Number</th>
                 <th class="text-right">Point Belanja</th>
-                <th>Status</th>
+                <th class="text-right">Status</th>
                 <th class="text-right">#</th>
             </tr>
         </thead>
@@ -25,7 +25,7 @@ $arrStatus = array(
                     <td>{{$hisReturn->purchase_number}}</td>
                     <td class="text-right">{{number_format($hisReturn->price,'0',',','.')}}</td>
                     <td class="text-right">
-                        <span class="text-success font-weight-bold">{{$arrStatus[$hisReturn->status]}}</span>
+                        <span class="bg-light border border-success pl-2 pr-2 pt-1 pb-1 rounded-pill">{{$arrStatus[$hisReturn->status]}}</span>
                     </td>
                     <td class="text-right">
                         @if($hisReturn->status == '2')
