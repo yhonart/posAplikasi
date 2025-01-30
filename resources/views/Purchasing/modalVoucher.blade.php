51fd95adcc0e2c6e6b4d8e2ff3b524091dadf2df
yhONdev <yusuf7789@gmail.com>
2025-01-30 23:24:55 +0700

    add card into modal

--- resources/views/Purchasing/modalVoucher.blade.php
+++ resources/views/Purchasing/modalVoucher.blade.php
@@ -1,47 +1,58 @@
-<div class="row">
-    <div class="col-md-12">
-        <h6 class="font-weight-bold text-muted">Riwayat Pengembalian/Retur Barang</h6>
+<div class="card card-indigo">
+    <div class="card-header">
+        <h3 class="card-title">Gunakan Voucher</h3>
     </div>
-</div>
-<div class="form-group row">
-    <div class="col-md-4">
-        <input type="text" name="point" id="point" class="form-control form-control-sm  text-success font-weight-bold" value="{{number_format($disPoint->NumRet,'0',',','.')}}" readonly>
-    </div>
-    <div class="col-md-8">
-        <div class="btn-group">
-            <button type="button" class="btn btn-outline-primary BTN-HARGA font-weight-bold" data-supplier = "{{$supID}}">Gunakan Semua Potongan Harga</button>
-            <button type="button" class="btn btn-outline-primary BTN-BARANG font-weight-bold" data-supplier = "{{$supID}}">Gunakan Penggantian Semua Barang</button>
+    <div class="card-body">
+        <div class="row">
+            <div class="col-md-12">
+                <h6 class="font-weight-bold text-muted">Riwayat Pengembalian/Retur Barang</h6>
+            </div>
+        </div>
+        <div class="form-group row">
+            <div class="col-md-4">
+                <input type="text" name="point" id="point" class="form-control form-control-sm  text-success font-weight-bold" value="{{number_format($disPoint->NumRet,'0',',','.')}}" readonly>
+            </div>
+            <div class="col-md-8">
+                <div class="btn-group">
+                    <button type="button" class="btn btn-outline-primary BTN-HARGA font-weight-bold" data-supplier = "{{$supID}}">Gunakan Semua Potongan Harga</button>
+                    <button type="button" class="btn btn-outline-primary BTN-BARANG font-weight-bold" data-supplier = "{{$supID}}">Gunakan Penggantian Semua Barang</button>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-12">
+                <table class="table table-sm table-hover table-valign-middle">
+                    <thead>
+                        <tr>
+                            <th>No.Pembelian</th>
+                            <th>Produk</th>
+                            <th>Qty.Pembelian</th>
+                            <th>Qty.Retur</th>
+                            <th>Hrg.Satuan</th>
+                            <th>Jml.Retur</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach($itemReturn as $iR)
+                            <tr>
+                                <td>{{$iR->purchase_number}}</td>
+                                <td>{{$iR->product_name}}</td>
+                                <td><span class="font-weight-bold text-info">{{$iR->received}}</span></td>
+                                <td><span class="font-weight-bold text-indigo">{{$iR->return}} {{$iR->unit}}</span></td>
+                                <td class="text-right">{{number_format($iR->unit_price,'0',',','.')}}</td>
+                                <td class="text-right">{{number_format($iR->total_price,'0',',','.')}}</td>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            </div>
         </div>
     </div>
-</div>
-<div class="row">
-    <div class="col-md-12">
-        <table class="table table-sm table-hover table-valign-middle">
-            <thead>
-                <tr>
-                    <th>No.Pembelian</th>
-                    <th>Produk</th>
-                    <th>Qty.Pembelian</th>
-                    <th>Qty.Retur</th>
-                    <th>Hrg.Satuan</th>
-                    <th>Jml.Retur</th>
-                </tr>
-            </thead>
-            <tbody>
-                @foreach($itemReturn as $iR)
-                    <tr>
-                        <td>{{$iR->purchase_number}}</td>
-                        <td>{{$iR->product_name}}</td>
-                        <td><span class="font-weight-bold text-info">{{$iR->received}}</span></td>
-                        <td><span class="font-weight-bold text-indigo">{{$iR->return}} {{$iR->unit}}</span></td>
-                        <td class="text-right">{{number_format($iR->unit_price,'0',',','.')}}</td>
-                        <td class="text-right">{{number_format($iR->total_price,'0',',','.')}}</td>
-                    </tr>
-                @endforeach
-            </tbody>
-        </table>
+    <div class="card-footer">
+        <span>Pilih cara penggunaan dengan benar</span>
     </div>
 </div>
+
 <script>
     $(document).ready(function(){
         $(".BTN-HARGA").on('click', function (e) {
