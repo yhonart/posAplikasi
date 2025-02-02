fc44aa0a9b8d93b05e1ce5df61c058fcfde56af2
yhONdev <yusuf7789@gmail.com>
2025-02-02 17:48:30 +0700

    change stayle dashboard

--- resources/views/Dashboard/DashboardLoadTrx.blade.php
+++ resources/views/Dashboard/DashboardLoadTrx.blade.php
@@ -74,7 +74,7 @@
 </div>
 
 <div class="row">
-    <div class="col-md-6">
+    <div class="col-md-8">
         <div class="card card-body">
             <div class="form-group row">
                 <div class="col-md-6">
@@ -92,8 +92,7 @@
             </div>
         </div>
     </div>  
-
-    <div class="col-md-6">
+    <div class="col-md-4">
         <div class="card card-info">
             <div class="card-header">
                 <h3 class="card-title">Transaksi Per Kasir</h3>
