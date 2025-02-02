9dc839b9536f553b0961db1451aefe61dece6a4d
yhONdev <yusuf7789@gmail.com>
2025-02-03 00:14:47 +0700

    alert

--- resources/views/Dashboard/DashboardLoadTrx.blade.php
+++ resources/views/Dashboard/DashboardLoadTrx.blade.php
@@ -4,6 +4,7 @@
     $dayNoww = date('l', strtotime($hariIni));
     $strHari = "Monday";
 ?>
+{{$getInfoKas}}
 @if($dayNoww == $strHari AND $getInfoKas <> '0')
 <div class="row mb-2">
     <div class="col-md-12">
