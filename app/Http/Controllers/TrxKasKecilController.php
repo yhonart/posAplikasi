2ce23921ee47c0cc6c7cb552b796e4a11d4cf3fc
yhONdev <yusuf7789@gmail.com>
2025-02-03 16:02:39 +0700

    change query

--- app/Http/Controllers/TrxKasKecilController.php
+++ app/Http/Controllers/TrxKasKecilController.php
@@ -34,7 +34,7 @@ class TrxKasKecilController extends Controller
     }
 
     public function tableLaporan($kasir, $fromDate, $endDate){
-        echo $fromDate."/".$endDate;
+        // echo $fromDate."/".$endDate;
         $firstDayThisWeek = $this->getMonday();
         // Tentukan hari pertama dari minggu sebelumnya
         $firstDayOfLastWeek = $firstDayThisWeek->copy()->subWeek();
@@ -76,7 +76,6 @@ class TrxKasKecilController extends Controller
                 ['kas_persCode',$kasir]
             ]);
         }
-        // $tablePengeluaran = $tablePengeluaran->where('trx_code','2');
         $tablePengeluaran = $tablePengeluaran->whereBetween('kas_date',[$fromDate,$endDate]);
         $tablePengeluaran = $tablePengeluaran->get();
 
