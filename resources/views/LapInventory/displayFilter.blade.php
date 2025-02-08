53833e6a5640dbbd3bb21bf50047cf44b0338670
yhONdev <yusuf7789@gmail.com>
2025-02-08 13:30:28 +0700

    change identification filter laporan inventory

--- resources/views/LapInventory/displayFilter.blade.php
+++ resources/views/LapInventory/displayFilter.blade.php
@@ -25,54 +25,55 @@
 <div class="row">
     <div class="col-12">
         <div class="card card-body table-responsive" style="height:700px;">
-            <table class="table table-valign-middle table-hover" id="tableDisplayLap">
-                <thead class="bg-gray-dark">
-                    <tr>
-                        <th>Tanggal {{$produk}}</th>
-                        <th>Nomor Bukti</th>
-                        <th>Product</th>
-                        <th>Keterangan</th>
-                        <th>Masuk</th>
-                        <th>Keluar</th>
-                        <th>Saldo</th>
-                    </tr>
-                </thead>
+            @if($codeDisplay == '1')
+                <!-- display table menggunakan filtering data  -->
+                <table class="table table-valign-middle table-hover" id="tableDisplayLap">
+                    <thead class="bg-gray-dark">
+                        <tr>
+                            <th>Tanggal</th>
+                            <th>Nomor Bukti</th>
+                            <th>Product</th>
+                            <th>Keterangan</th>
+                            <th>Masuk</th>
+                            <th>Keluar</th>
+                            <th>Saldo</th>
+                        </tr>
+                    </thead>
+                </table>
                 <tbody>
                     <?php                           
                         $saldoawal = 0; 
                         $saldoHitung = 0;                       
-                        if ($codeDisplay == '1') {
-                            if (!empty($dataSaldoAwal)) {
-                                echo "<tr>";
-                                    echo "<td>".$dataSaldoAwal->date_input."</td>";
-                                    echo "<td></td>";
-                                    echo "<td></td>";
-                                    echo "<td>Saldo Awal</td>";
-                                    echo "<td class='text-right'>0</td>";
-                                    echo "<td class='text-right'>0</td>";
-                                    echo "<td class='text-right'>";
-    
-                                    if ($dataSaldoAwal->inv_in == '0') {
-                                        $saldoawal = $dataSaldoAwal->saldo + $dataSaldoAwal->inv_out;
-                                    }
-                                    else {
-                                        $saldoawal = $dataSaldoAwal->saldo - $dataSaldoAwal->inv_in;
-                                    }
-    
-                                    if ($mProduct->medium_unit_val == '0') {
-                                        $awal = $saldoawal;
-                                    }
-                                    elseif ($mProduct->small_unit_val == '0') {
-                                        $awal = $saldoawal * $mProduct->medium_unit_val;
-                                    }
-                                    else {
-                                        $awal = $saldoawal * $mProduct->small_unit_val;
-                                    }
-                                    echo $saldoawal;
-                                    echo "</td>";
-                                echo "</tr>";
-                            }
-                        }                   
+                        if (!empty($dataSaldoAwal)) {
+                            echo "<tr>";
+                                echo "<td>".$dataSaldoAwal->date_input."</td>";
+                                echo "<td></td>";
+                                echo "<td></td>";
+                                echo "<td>Saldo Awal</td>";
+                                echo "<td class='text-right'>0</td>";
+                                echo "<td class='text-right'>0</td>";
+                                echo "<td class='text-right'>";
+
+                                if ($dataSaldoAwal->inv_in == '0') {
+                                    $saldoawal = $dataSaldoAwal->saldo + $dataSaldoAwal->inv_out;
+                                }
+                                else {
+                                    $saldoawal = $dataSaldoAwal->saldo - $dataSaldoAwal->inv_in;
+                                }
+
+                                if ($mProduct->medium_unit_val == '0') {
+                                    $awal = $saldoawal;
+                                }
+                                elseif ($mProduct->small_unit_val == '0') {
+                                    $awal = $saldoawal * $mProduct->medium_unit_val;
+                                }
+                                else {
+                                    $awal = $saldoawal * $mProduct->small_unit_val;
+                                }
+                                echo $saldoawal;
+                                echo "</td>";
+                            echo "</tr>";
+                        }                  
                     ?>
                     @foreach($dataReportInv as $dri)
                         <tr>
@@ -99,7 +100,32 @@
                             <td class="bg-lightblue"></td>
                         </tr>
                 </tbody>
-            </table>
+            @else
+                <table class="table table-valign-middle table-hover">
+                    <thead class="bg-gray-dark">
+                        <tr>
+                            <th>Tanggal</th>
+                            <th>Nomor Bukti</th>
+                            <th>Product</th>
+                            <th>Keterangan</th>
+                            <th>Masuk</th>
+                            <th>Keluar</th>
+                        </tr>
+                    </thead>
+                </table>
+                <tbody>
+                    @foreach($dataReportInv as $dri)
+                        <tr>
+                            <td>{{$dri->date_input}}</td>
+                            <td>{{$dri->number_code}}</td>
+                            <td>{{$dri->product_name}}</td>
+                            <td>{{$dri->description}}</td>
+                            <td class="text-right">{{$dri->inv_in}}</td>
+                            <td class="text-right">{{$dri->inv_out}}</td>                            
+                        </tr>
+                    @endforeach
+                </tbody>
+            @endif            
         </div>
     </div>
 </div>
