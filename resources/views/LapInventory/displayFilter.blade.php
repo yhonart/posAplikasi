a2ba347f8a768b59d0e4530482e05e4a9ee95771
yhONdev <yusuf7789@gmail.com>
2025-02-08 13:43:33 +0700

    change p-4

--- resources/views/LapInventory/displayFilter.blade.php
+++ resources/views/LapInventory/displayFilter.blade.php
@@ -101,8 +101,8 @@
                     </tbody>
                 </table>
             @else
-                <div class="blue-alert rounded mb-2">
-                    <span class="p-4 font-weight-bold"><i class="fa-solid fa-circle-info"></i> Secara default, data yang di tampilkan adalah transaksi data hari ini. Silahkan gunakan filter data untuk menampilkan per item.</span>
+                <div class="blue-alert rounded mb-2 p-4">
+                    <span class="font-weight-bold"><i class="fa-solid fa-circle-info"></i> Secara default, data yang di tampilkan adalah transaksi data hari ini. Silahkan gunakan filter data untuk menampilkan per item.</span>
                 </div>
                 <table class="table table-valign-middle table-hover">
                     <thead class="bg-gray-dark">
