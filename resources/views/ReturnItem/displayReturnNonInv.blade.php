b940d09ff4dbc228c72d8f67e9bdb0d5e3ef1925
yhONdev <yusuf7789@gmail.com>
2025-02-12 09:31:16 +0700

    add form return non invoice

--- resources/views/ReturnItem/displayReturnNonInv.blade.php
+++ resources/views/ReturnItem/displayReturnNonInv.blade.php
@@ -4,17 +4,39 @@
     </div>
     <div class="card-body">
         @if($countNumberRetur == '0')
-            <div class="form-group row">
-                <label for="supplier" class="col-md-3"> Supplier</label>
-                <div class="col-md-4">
-                    <select name="supplier" id="supplier" class="form-control form-control-sm">
-                        <option value="0"> ==== </option>
-                        @foreach($optionSupplier as $ops)
-                            <option value="{{$ops->idm_supplier}}">{{$ops->store_name}}</option>
-                        @endforeach
-                    </select>
+            <form id="formCreateDokRetur">
+                <div class="form-group row">
+                    <label for="numberDokumen" class="col-md-3">No. Dokumen</label>
+                    <div class="col-md-4">
+                        <input type="text" class="form-control form-control-sm" name="numberDokumen" id="numberDokumen">
+                    </div>
                 </div>
-            </div>
+                <div class="form-group row">
+                    <label for="tglDokumen" class="col-md-3">No. Dokumen</label>
+                    <div class="col-md-4">
+                        <input type="text" class="form-control form-control-sm" name="tglDokumen" id="tglDokumen">
+                    </div>
+                </div>
+                <div class="form-group row">
+                    <label for="supplier" class="col-md-3"> Supplier</label>
+                    <div class="col-md-4">
+                        <select name="supplier" id="supplier" class="form-control form-control-sm">
+                            <option value="0"> ==== </option>
+                            @foreach($optionSupplier as $ops)
+                                <option value="{{$ops->idm_supplier}}">{{$ops->store_name}}</option>
+                            @endforeach
+                        </select>
+                    </div>
+                </div>
+                <div class="form-group row">
+                    <div class="col-md-12">
+                        <button type="submit" class="btn btn-sm btn-success font-weight-bold"><i class="fa-solid fa-floppy-disk"></i> Simpan</button>
+                    </div>
+                </div>
+            </form>
+            <script>
+
+            </script>
         @else
             <div id="transaksiReturNonInvoice"></div>
         @endif
