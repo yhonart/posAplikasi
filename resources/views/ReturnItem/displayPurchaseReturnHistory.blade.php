b164d3610b7a55a6b97b9adab2e47fce5b36bbe2
yhONdev <yusuf7789@gmail.com>
2025-02-13 09:00:32 +0700

    change change card list return

--- resources/views/ReturnItem/displayPurchaseReturnHistory.blade.php
+++ resources/views/ReturnItem/displayPurchaseReturnHistory.blade.php
@@ -8,36 +8,41 @@ $arrStatus = array(
     4=>"Sudah Digunakan"
 );
 ?>
-<div class="card card-body">
-    <table class="table table-valign-middle table-hover">
-        <thead class="bg-gray-dark">
-            <tr>
-                <th>Supplier</th>
-                <th>Purchase Number</th>
-                <th class="text-right">Point Belanja</th>
-                <th class="text-right">Status</th>
-                <th class="text-right">#</th>
-            </tr>
-        </thead>
-        <tbody>
-            @foreach($historyReturn as $hisReturn)
+<div class="card card-purple">
+    <div class="card-header border-0">
+        <h3 class="card-title font-weight-bold">History Return</h3>
+    </div>
+    <div class="card-body">
+        <table class="table table-valign-middle table-hover">
+            <thead class="bg-gray-dark">
                 <tr>
-                    <td>{{$hisReturn->store_name}}</td>
-                    <td>{{$hisReturn->purchase_number}}</td>
-                    <td class="text-right">{{number_format($hisReturn->price,'0',',','.')}}</td>
-                    <td class="text-right">
-                        <span class="bg-light border border-success pl-2 pr-2 pt-1 pb-1 rounded-pill">{{$arrStatus[$hisReturn->status]}}</span>
-                    </td>
-                    <td class="text-right">
-                        @if($hisReturn->status == '2')
-                            <button type="button" class="btn btn-sm btn-outline-success BTN-APPROVE font-weight-bold" data-purchase="{{$hisReturn->purchase_number}}">Approve</button>
-                        @endif
-                        <button type="button" class="btn btn-sm btn-outline-info BTN-DETAIL font-weight-bold" data-purchase="{{$hisReturn->purchase_number}}">Detail</button>
-                    </td>
+                    <th>Supplier</th>
+                    <th>Purchase Number</th>
+                    <th class="text-right">Point Belanja</th>
+                    <th class="text-right">Status</th>
+                    <th class="text-right">#</th>
                 </tr>
-            @endforeach
-        </tbody>
-    </table>
+            </thead>
+            <tbody>
+                @foreach($historyReturn as $hisReturn)
+                    <tr>
+                        <td>{{$hisReturn->store_name}}</td>
+                        <td>{{$hisReturn->purchase_number}}</td>
+                        <td class="text-right">{{number_format($hisReturn->price,'0',',','.')}}</td>
+                        <td class="text-right">
+                            <span class="bg-light border border-success pl-2 pr-2 pt-1 pb-1 rounded-pill">{{$arrStatus[$hisReturn->status]}}</span>
+                        </td>
+                        <td class="text-right">
+                            @if($hisReturn->status == '2')
+                                <button type="button" class="btn btn-sm btn-outline-success BTN-APPROVE font-weight-bold" data-purchase="{{$hisReturn->purchase_number}}">Approve</button>
+                            @endif
+                            <button type="button" class="btn btn-sm btn-outline-info BTN-DETAIL font-weight-bold" data-purchase="{{$hisReturn->purchase_number}}">Detail</button>
+                        </td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
 </div>
 <script>
     $(document).ready(function(){
