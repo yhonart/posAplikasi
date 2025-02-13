c571f6a3f47559594ffa5de7adeeca5a2a8de9f1
yhONdev <yusuf7789@gmail.com>
2025-02-13 22:57:47 +0700

    add kredit limit

--- resources/views/Cashier/cashierModalPembayaran.blade.php
+++ resources/views/Cashier/cashierModalPembayaran.blade.php
@@ -17,7 +17,7 @@
     <div class="col-12">
         <div class="card card-purple">
             <div class="card-header border-0">
-                <h3 class="card-title font-weight-bold">{{$dataBilling->customer_name}}</h3>
+                <h3 class="card-title font-weight-bold">{{$dataBilling->customer_name}} Kredit Limi : {{$dataBilling->kredit_limit}}</h3>
                 <div class="card-tools">
                     <button type="button" class="btn btn-tool border-0 elevation-1" data-dismiss="modal"><i class="fa-solid fa-xmark"></i></button>
                 </div>
