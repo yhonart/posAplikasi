dc93c6bebf8326cf8215dd965d19ea124b44bb8a
yhONdev <yusuf7789@gmail.com>
2025-02-13 16:01:43 +0700

    change ordering

--- resources/views/ReturnItem/displayPurchaseSearchList.blade.php
+++ resources/views/ReturnItem/displayPurchaseSearchList.blade.php
@@ -40,6 +40,7 @@
             "autoWidth": false,
             "dom": 'Bfrtip',
             "paging": false,
+            "ordering": false,
             "buttons": ["copy", "csv", "excel", "pdf", "print"]
         }).buttons().container().appendTo('#example1_wrapper .col-md-6:eq(0)');
     });
