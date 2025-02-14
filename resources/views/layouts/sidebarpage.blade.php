e1297132b85f32c3e4079a41e3f95a7a732dc121
yhONdev <yusuf7789@gmail.com>
2025-02-14 22:47:40 +0700

    change logout when close tab

--- resources/views/layouts/sidebarpage.blade.php
+++ resources/views/layouts/sidebarpage.blade.php
@@ -131,7 +131,20 @@
           timer: 3000
         });
         window.addEventListener('beforeunload', function (e) {
-            navigator.sendBeacon("{{ route('logout') }}", '_token=' + document.querySelector('meta[name="csrf-token"]').getAttribute('content'));
+            // Hanya jalankan jika bukan reload halaman
+            if (!navigator.sendBeacon) {
+                //gunakan ajax biasa jika browser tidak support sendBeacon
+                $.ajax({
+                type: 'POST',
+                url: "{{ route('logout') }}", // Ganti dengan route yang Anda inginkan
+                data: {
+                    _token: $('meta[name="csrf-token"]').attr('content') // Tambahkan CSRF token Laravel
+                },
+                async: false // Penting untuk memastikan permintaan selesai sebelum halaman ditutup
+                });
+            } else {
+                navigator.sendBeacon("{{ route('logout') }}", new FormData(document.querySelector('form')));
+            }
         });
         
         // setTimeout(function () {
