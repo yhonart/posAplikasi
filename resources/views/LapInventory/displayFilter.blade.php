d09664065912f5c1c2220c812db18523ef2d3f5a
yhONdev <yusuf7789@gmail.com>
2025-02-15 12:21:58 +0700

    change iden saldo awal

--- resources/views/LapInventory/displayFilter.blade.php
+++ resources/views/LapInventory/displayFilter.blade.php
@@ -69,7 +69,7 @@
                                     else {
                                         $awal = $saldoawal * $mProduct->small_unit_val;
                                     }
-                                    echo $saldoawal;
+                                    echo $dataSaldoAwal->inv_in;
                                     $saldo = $saldoawal;
                                     echo "</td>";
                                 echo "</tr>";
