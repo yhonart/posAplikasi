3218d949de03771c0c3722051339a1058986daee
yhONdev <yusuf7789@gmail.com>
2025-02-20 22:56:31 +0700

    change display table dok opname

--- resources/views/StockOpname/tableDokOpname.blade.php
+++ resources/views/StockOpname/tableDokOpname.blade.php
@@ -38,7 +38,7 @@
                     @endif
                     
                     @if($approval >= '1' AND $summary->status == '2' AND $summary->t_input_stock<>'')
-                        <button type="button" class="btn btn-sm btn-success btnApprove elevation-1 " title="Approve" data-opname="{{$summary->number_so}}"><i class="fa-solid fa-check"></i></button>
+                        <button type="button" class="btn btn-sm btn-success btnApprove elevation-1 " title="Approve" data-opname="{{$summary->number_so}}"><i class="fa-solid fa-check"></i> Setujui</button>
                     @endif
                     @if($approval >= '1' AND $approval >='1')
                         <button type="button" class="btn btn-sm btn-primary btnDetail elevation-1 " id="btnDetail" title="View Detail" data-opname="{{$summary->number_so}}">
@@ -46,7 +46,7 @@
                         </button>
                     @endif
                     @if($summary->status == '2')
-                        <button type="button" class="btn btn-sm btn-danger btnDelete elevation-1 " title="Delete" data-opname="{{$summary->number_so}}"><i class="fa-solid fa-trash"></i></button>
+                        <button type="button" class="btn btn-sm btn-danger btnDelete elevation-1 " title="Delete" data-opname="{{$summary->number_so}}"><i class="fa-solid fa-trash"></i> Hapus</button>
                     @endif
                 </td>
             </tr>
