49a93cf09cebe95962d3e7acf111e3408538c236
yhONdev <yusuf7789@gmail.com>
2025-02-20 22:37:59 +0700

    testing tooltip

--- resources/views/StockOpname/tableDokOpname.blade.php
+++ resources/views/StockOpname/tableDokOpname.blade.php
@@ -41,7 +41,10 @@
                         <button type="button" class="btn btn-sm btn-success btnApprove elevation-1 " title="Approve" data-opname="{{$summary->number_so}}"><i class="fa-solid fa-check"></i></button>
                     @endif
                     @if($approval >= '1' AND $approval >='1')
-                        <button type="button" class="btn btn-sm btn-primary btnDetail elevation-1 " id="btnDetail" title="View Detail" data-opname="{{$summary->number_so}}"><i class="fa-solid fa-magnifying-glass"></i></button>
+                        <button type="button" class="btn btn-sm btn-primary btnDetail elevation-1 " id="btnDetail" title="View Detail" data-opname="{{$summary->number_so}}">
+                            <i class="fa-solid fa-magnifying-glass"></i>
+                            <span class="tooltiptext">Ini adalah tooltip!</span>
+                        </button>
                     @endif
                     @if($summary->status == '2')
                         <button type="button" class="btn btn-sm btn-danger btnDelete elevation-1 " title="Delete" data-opname="{{$summary->number_so}}"><i class="fa-solid fa-trash"></i></button>
