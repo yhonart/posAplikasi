ebdac86463e464ae7723e9f82ac7ac6e830453f8
yhONdev <yusuf7789@gmail.com>
2025-02-23 08:13:32 +0700

    change color status

--- resources/views/StockOpname/tableDokOpname.blade.php
+++ resources/views/StockOpname/tableDokOpname.blade.php
@@ -37,8 +37,8 @@
                 <td>{{$summary->number_so}}</td>
                 <td>{{$summary->site_name}}</td>
                 <td>{{$summary->created_by}}</td>
-                <td>
-                    <span class="badge {{$bgColor[$summary->status]}}">{{$araystatus[$summary->status]}}</span>                    
+                <td>                    
+                    <span class="{{$bgColor[$summary->status]}} pl-2 pr-2 pt-1 pb-1 rounded-pill font-weight-bold text-xs">{{$araystatus[$summary->status]}}</span>
                 </td>
                 <td class="text-right">
                     @if($summary->status == '2')
