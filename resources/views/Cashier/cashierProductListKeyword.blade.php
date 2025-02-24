f2c5d6ccfe1d91ed02e06a34488089d6207e1ba3
yhONdev <yusuf7789@gmail.com>
2025-02-24 10:44:23 +0700

    add text primary

--- resources/views/Cashier/cashierProductListKeyword.blade.php
+++ resources/views/Cashier/cashierProductListKeyword.blade.php
@@ -1,7 +1,7 @@
 @foreach($productList as $pL)
 <tr>
     <td colspan="2" class="p-0">
-        <button class="btn btn-default btn-block rounded-0 border-0 onClick-produk elevation-0 btn-sm" data-id="{{$pL->idm_product_satuan}}">{{$pL->product_name}}</button>
+        <button class="btn btn-default btn-block rounded-0 border-0 onClick-produk elevation-0 btn-sm text-primary font-weight-bold" data-id="{{$pL->idm_product_satuan}}">{{$pL->product_name}}</button>
     </td>
     <td class="p-0"></td class="p-0">
     <td class="p-0">
@@ -10,7 +10,7 @@
     <td class="text-right p-0">
         @foreach($getPrice as $gp)
             @if($gp->core_product_price == $pL->idm_data_product AND $gp->size_product == $pL->product_size)
-                {{number_format($gp->price_sell,'0',',','.')}} {{$gp->cos_group}}
+                {{number_format($gp->price_sell,'0',',','.')}}
             @endif
         @endforeach
     </td>
