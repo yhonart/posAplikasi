857e28bfb4c3e5beee8bb510699e5c61c6313276
yhONdev <yusuf7789@gmail.com>
2025-02-24 09:53:59 +0700

    changes tr date

--- resources/views/Cashier/cashierProductListKeyword.blade.php
+++ resources/views/Cashier/cashierProductListKeyword.blade.php
@@ -1,15 +1,15 @@
 @foreach($productList as $pL)
 <tr>
-    <td>
+    <td colspan="2" class="p-0">
         <button class="btn btn-default btn-block rounded-0 border-0 onClick-produk" data-id="{{$pL->idm_product_satuan}}">{{$pL->product_name}}</button>
     </td>
-    <td>
+    <td class="p-0">
         <input type="number" name="loadQty" id="loadQty" class="form-control form-control-sm form-control-border border-width-2" autocomplete="off" readonly>
-    </td>
-    <td>
+    </td class="p-0">
+    <td class="p-0">
         {{$pL->product_satuan}}
-    </td>
-    <td>
+    </td class="p-0">
+    <td class="p-0">
         {{number_format($pL->product_price_order,'0',',','.')}}
     </td>
 </tr>
