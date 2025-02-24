5a4895ccd2eefa328d7bede292444dfda049c74c
yhONdev <yusuf7789@gmail.com>
2025-02-24 10:32:16 +0700

    change price

--- resources/views/Cashier/cashierProductListKeyword.blade.php
+++ resources/views/Cashier/cashierProductListKeyword.blade.php
@@ -9,7 +9,7 @@
     </td class="p-0">
     <td class="text-right p-0">
         @foreach($getPrice as $gp)
-            @if($gp->core_product_price == $pL->idm_product_satuan AND $gp->size_product == $pL->product_size)
+            @if($gp->core_product_price == $pL->idm_product_satuan)
                 {{number_format($gp->price_sell,'0',',','.')}}
             @endif
         @endforeach
