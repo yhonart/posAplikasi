ce07f481644936f64c808ecef303a9aa2a7e302a
yhONdev <yusuf7789@gmail.com>
2025-02-25 14:54:47 +0700

    remove product id

--- resources/views/Cashier/cashierProductListKeyword.blade.php
+++ resources/views/Cashier/cashierProductListKeyword.blade.php
@@ -1,7 +1,7 @@
 @foreach($productList as $pL)
 <tr>
     <td colspan="2" class="p-0">
-        <button class="btn btn-default btn-block rounded-0 border-0 onClick-produk elevation-0 btn-sm text-primary font-weight-bold text-left" data-id="{{$pL->idinv_stock}}">{{$pL->product_name}} - {{$pL->product_id}}</button>
+        <button class="btn btn-default btn-block rounded-0 border-0 onClick-produk elevation-0 btn-sm text-primary font-weight-bold text-left" data-id="{{$pL->idinv_stock}}">{{$pL->product_name}}</button>
     </td>
     <td class="p-0"></td class="p-0">
     <td class="p-0">
