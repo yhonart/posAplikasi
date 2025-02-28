a2d7f36b0102c8bcfc724d609de1531bcc18f450
yhONdev <yusuf7789@gmail.com>
2025-02-28 22:43:37 +0700

    test hightlight table row arrows keys

--- resources/views/Cashier/cashierProductList.blade.php
+++ resources/views/Cashier/cashierProductList.blade.php
@@ -52,7 +52,56 @@
 </table>
 <script type="text/javascript">
      $(function () {   
-        $("#fieldProduk").val(null).focus();        
+        $("#fieldProduk").val(null).focus();
+        var active = 0;
+        $('#tableSelectProduk td').each(function(idx){$(this).html(idx);});
+        rePosition();
+
+        $(document).keydown(function(e){
+            reCalculate(e);
+            rePosition();
+            return false;
+        });
+
+        $('td').click(function(){
+            active = $(this).closest('table').find('td').index(this);
+            rePosition();
+        });
+        function reCalculate(e){
+            var rows = $('#navigate tr').length;
+            var columns = $('#navigate tr:eq(0) td').length;
+            //alert(columns + 'x' + rows);
+            
+            if (e.keyCode == 37) { //move left or wrap
+                active = (active>0)?active-1:active;
+            }
+            if (e.keyCode == 38) { // move up
+                active = (active-columns>=0)?active-columns:active;
+            }
+            if (e.keyCode == 39) { // move right or wrap
+            active = (active<(columns*rows)-1)?active+1:active;
+            }
+            if (e.keyCode == 40) { // move down
+                active = (active+columns<=(rows*columns)-1)?active+columns:active;
+            }
+        }
+
+        function rePosition(){
+            $('.active').removeClass('active');
+            $('#navigate tr td').eq(active).addClass('active');
+            scrollInView();
+        }
+
+        function scrollInView(){
+            var target = $('#navigate tr td:eq('+active+')');
+            if (target.length)
+            {
+                var top = target.offset().top;
+                
+                $('html,body').stop().animate({scrollTop: top-100}, 400);
+                return false;
+            }
+        }
     });   
     $(document).ready(function() {
         let keyword = '0',
