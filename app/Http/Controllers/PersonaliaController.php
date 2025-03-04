0605131a8e2e76a401eef99bfcd02d3c46ee7e37
yhONdev <yusuf7789@gmail.com>
2025-03-05 06:00:53 +0700

    change authHakAkses

--- app/Http/Controllers/PersonaliaController.php
+++ app/Http/Controllers/PersonaliaController.php
@@ -200,7 +200,7 @@ class PersonaliaController extends Controller
     }
     
     public function searchData ($keyword){
-        $hakAkses = Auth::user()->hakakses;
+        $authHakAkses = Auth::user()->hakakses;
         $users = DB::table('users as a');
         $users = $users->select('a.*','b.*','c.*','d.site_name');
         $users = $users->leftJoin('users_area as b','a.id','=','b.user_id');
@@ -211,7 +211,7 @@ class PersonaliaController extends Controller
             }
         $users = $users->get();
 
-        return view ('hris/masterData/personaliaList', compact('users','hakAkses'));
+        return view ('hris/masterData/personaliaList', compact('users','authHakAkses'));
     }
     
     public function changeHakAkses (Request $reqChange){
