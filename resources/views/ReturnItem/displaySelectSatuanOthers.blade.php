329043c4860af4f192389a59b006a0ee566dfc7e
yhONdev <yusuf7789@gmail.com>
2025-03-23 13:38:17 +0700

    change display satuan

--- resources/views/ReturnItem/displaySelectSatuanOthers.blade.php
+++ resources/views/ReturnItem/displaySelectSatuanOthers.blade.php
@@ -1,8 +1,5 @@
 <select class="form-control form-control-sm " name="satuan" id="satuan">
     @foreach($satuanItem as $itemSatuan)
         <option value="{{$itemSatuan->product_size}}">{{$itemSatuan->product_satuan}}</option>
-    @endforeach
-    @foreach($otherSize as $os)
-        <option value="{{$os->unit_note}}">{{$os->unit_note}}</option>
-    @endforeach
+    @endforeach    
 </select>
