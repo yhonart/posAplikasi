39ddbfab464a47e1ba872710dc8e54a19a1a03fb
yhONdev <yusuf7789@gmail.com>
2025-03-24 15:07:40 +0700

    changes table

--- resources/views/ReturnItem/displayReturnNonInvTableItem.blade.php
+++ resources/views/ReturnItem/displayReturnNonInvTableItem.blade.php
@@ -1,18 +1,16 @@
-<tbody>
-    @foreach($getItem as $gI)
-        <tr>
-            <td>{{$gI->product_name}}</td>
-            <td>{{$gI->unit}}</td>
-            <td>{{$gI->site_name}}</td>
-            <td>{{$gI->return}}</td>
-            <td>{{$gI->unit_price}}</td>
-            <td>{{$gI->total_price}}</td>
-            <td>{{$gI->stock_awal}}</td>
-            <td>{{$gI->stock_akhir}}</td>
-            <td>{{$gI->item_text}}</td>
-            <td>
-                <button type="button" class="btn btn-sm btn-danger btn-block btn-flat"><i class="fa-solid fa-xmark"></i></button>
-            </td>
-        </tr>
-    @endforeach
-</tbody>
+@foreach($getItem as $gI)
+    <tr>
+        <td>{{$gI->product_name}}</td>
+        <td>{{$gI->unit}}</td>
+        <td>{{$gI->site_name}}</td>
+        <td>{{$gI->return}}</td>
+        <td>{{$gI->unit_price}}</td>
+        <td>{{$gI->total_price}}</td>
+        <td>{{$gI->stock_awal}}</td>
+        <td>{{$gI->stock_akhir}}</td>
+        <td>{{$gI->item_text}}</td>
+        <td>
+            <button type="button" class="btn btn-sm btn-danger btn-block btn-flat"><i class="fa-solid fa-xmark"></i></button>
+        </td>
+    </tr>
+@endforeach
