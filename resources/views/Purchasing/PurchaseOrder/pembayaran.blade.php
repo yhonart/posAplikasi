eb8c005948219be4f508aeab495c2a7dcaa22c93
yhONdev <yusuf7789@gmail.com>
2025-03-26 09:31:40 +0700

    change display pembayaran hutang supplier

--- resources/views/Purchasing/PurchaseOrder/pembayaran.blade.php
+++ resources/views/Purchasing/PurchaseOrder/pembayaran.blade.php
@@ -1,42 +1,39 @@
-<div class="card">
-    <div class="card-header border-0">
-        <h3 class="card-title">Input Pembayaran <small class="text-muted">* Pilih nama supplier untuk input pembayaran</small></h3>
+<div class="row">
+    <div class="col-md-12">
+        <span>*Pilih nama supplier untuk melakukan pembayaran hutang</span>
     </div>
-    <div class="card-body">
-        <div class="row">
-            <div class="col-md-3">
-                <div class="form-group">
-                    <label for="supName" class="label">Supplier</label>
-                    <select name="supName" id="supName" class="form-control form-control-sm">
-                        <option value="0">====</option>
-                        @foreach($mSupplier as $msup)
-                        <option value="{{$msup->idm_supplier}}">{{$msup->store_name}}</option>
-                        @endforeach
-                    </select>
-                </div>
-            </div>
-            <div class="col-md-3">
-                <div class="form-group">
-                    <label for="dariTanggal" class="label">Dari Tanggal</label>
-                    <input type="text" class="form-control datetimepicker-input" name="dariTanggal" id="dariTanggal">
-                </div>
-            </div>
-            <div class="col-md-3">
-                <div class="form-group">
-                    <label for="sampaiTanggal" class="label">Sampai Tanggal</label>
-                    <input type="text" class="form-control datetimepicker-input roundedd-0" name="sampaiTanggal" id="sampaiTanggal">
-                </div>
-            </div>
+</div>
+<div class="row">
+    <div class="col-md-3">
+        <div class="form-group">
+            <label for="supName" class="label">Supplier</label>
+            <select name="supName" id="supName" class="form-control form-control-sm">
+                <option value="0">====</option>
+                @foreach($mSupplier as $msup)
+                <option value="{{$msup->idm_supplier}}">{{$msup->store_name}}</option>
+                @endforeach
+            </select>
         </div>
-        
-        <div class="row">
-            <div class="col-md-12">
-                <div id="tableFilter"></div>
-            </div>
+    </div>
+    <div class="col-md-3">
+        <div class="form-group">
+            <label for="dariTanggal" class="label">Dari Tanggal</label>
+            <input type="text" class="form-control datetimepicker-input" name="dariTanggal" id="dariTanggal">
+        </div>
+    </div>
+    <div class="col-md-3">
+        <div class="form-group">
+            <label for="sampaiTanggal" class="label">Sampai Tanggal</label>
+            <input type="text" class="form-control datetimepicker-input roundedd-0" name="sampaiTanggal" id="sampaiTanggal">
         </div>
     </div>
 </div>
 
+<div class="row">
+    <div class="col-md-12">
+        <div id="tableFilter"></div>
+    </div>
+</div>
 
 <script>
     $(function(){
