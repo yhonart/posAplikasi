88658e4f35306d623d68c047033b374f8c84fb30
yhONdev <yusuf7789@gmail.com>
2025-03-26 09:27:07 +0700

    change display pembayaran customer

--- resources/views/HutangCustomers/pembayaran.blade.php
+++ resources/views/HutangCustomers/pembayaran.blade.php
@@ -1,53 +1,51 @@
-<div class="card"> 
-    <div class="card-header border-0">
-        <h3 class="card-title">Input Pembayaran <small class="text-muted">* Pilih nama pelanggan untuk input pembayaran</small></h3>
-    </div>                   
-    <div class="card-body">                
-        <div class="row">
-            <div class="col-6 col-md-3">
-                <div class="form-group">
-                    <label class="form-label">Cari Nama Pelanggan</label>
-                    <select class="form-control select-pelanggan" id="cariNamaPelanggan" class="form-control ">
-                        <option value="0" readonly>Nama Pelanggan</option>
-                        @foreach($dbMCustomer as $dcs)
-                        <option value="{{$dcs->idm_customer }}">{{$dcs->customer_store}}</option>
-                        @endforeach
-                    </select>
-                </div>
-            </div>
-
-            <div class="col-6 col-md-3">
-                <div class="form-group">
-                    <label class="form-label">Dari Tanggal</label>
-                    <input type="text" class="form-control datetimepicker-input" name="dariTanggal" id="dariTanggal">
-                </div>
-            </div>
-
-            <div class="col-6 col-md-3">
-                <div class="form-group">
-                    <label class="form-label">s.d Tanggal</label>
-                    <input type="text" class="form-control datetimepicker-input roundedd-0" name="sampaiTanggal" id="sampaiTanggal">
-                </div>
-            </div>
+<div class="row">
+    <div class="col-md-12">
+        <p class="text-muted">* Pilih nama pelanggan untuk input pembayaran hutang</p>
+    </div>
+</div>
+<div class="row">
+    <div class="col-6 col-md-3">
+        <div class="form-group">
+            <label class="form-label">Cari Nama Pelanggan</label>
+            <select class="form-control select-pelanggan" id="cariNamaPelanggan" class="form-control ">
+                <option value="0" readonly>Nama Pelanggan</option>
+                @foreach($dbMCustomer as $dcs)
+                <option value="{{$dcs->idm_customer }}">{{$dcs->customer_store}}</option>
+                @endforeach
+            </select>
         </div>
-        <div class="row mb-2">
-            <div class="col-12">
-                <div id="reloadDisplay" style="display: none;">
-                    <div class="spinner-grow text-dark spinner-grow-sm" role="status">
-                        <span class="sr-only">Loading...</span>
-                    </div>
-                    <span>Please Wait ....</span>
-                </div>
-            </div>
+    </div>
+
+    <div class="col-6 col-md-3">
+        <div class="form-group">
+            <label class="form-label">Dari Tanggal</label>
+            <input type="text" class="form-control datetimepicker-input" name="dariTanggal" id="dariTanggal">
         </div>
-        <div class="row">
-            <div class="col-md-12">
-                <a href="{{asset('public/Document/ManualBookPembayaranHutang.pdf')}}" class="btn bg-light border-0" target="_blank">Dokumentasi Penggunaan.</a>
+    </div>
+
+    <div class="col-6 col-md-3">
+        <div class="form-group">
+            <label class="form-label">s.d Tanggal</label>
+            <input type="text" class="form-control datetimepicker-input roundedd-0" name="sampaiTanggal" id="sampaiTanggal">
+        </div>
+    </div>
+</div>
+<div class="row mb-2">
+    <div class="col-12">
+        <div id="reloadDisplay" style="display: none;">
+            <div class="spinner-grow text-dark spinner-grow-sm" role="status">
+                <span class="sr-only">Loading...</span>
             </div>
+            <span>Please Wait ....</span>
         </div>
     </div>
 </div>
 <div id="divDataPelunasan"></div>
+<div class="row">
+    <div class="col-md-12">
+        <a href="{{asset('public/Document/ManualBookPembayaranHutang.pdf')}}" class="btn bg-light border-0" target="_blank">Dokumentasi Penggunaan.</a>
+    </div>
+</div>
 <script>
     $(function() {
         $( ".datetimepicker-input" ).datepicker({
