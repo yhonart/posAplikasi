bf235155654427e269f10cb0649a394e2e2ef0c0
yhONdev <yusuf7789@gmail.com>
2025-03-29 06:50:27 +0700

    add change box dana

--- resources/views/TrxKasKecil/mainBoxContentDana.blade.php
+++ resources/views/TrxKasKecil/mainBoxContentDana.blade.php
@@ -2,7 +2,23 @@
     <div class="col-12 col-sm-4">
         <div class="info-box bg-light">
             <div class="info-box-content">
-                <span class="info-box-text text-center text-muted">Dana Tersedia</span>
+                <span class="info-box-text text-center text-muted">Dana Masuk</span>
+                <span class="info-box-number text-center text-muted mb-0">2300</span>
+            </div>
+        </div>
+    </div>
+    <div class="col-12 col-sm-4">
+        <div class="info-box bg-light">
+            <div class="info-box-content">
+                <span class="info-box-text text-center text-muted">Dana Keluar</span>
+                <span class="info-box-number text-center text-muted mb-0">2300</span>
+            </div>
+        </div>
+    </div>
+    <div class="col-12 col-sm-4">
+        <div class="info-box bg-light">
+            <div class="info-box-content">
+                <span class="info-box-text text-center text-muted">Saldo Dana</span>
                 <span class="info-box-number text-center text-muted mb-0">2300</span>
             </div>
         </div>
