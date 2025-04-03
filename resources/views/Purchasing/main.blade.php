77b97dab88bc55b504e7cc6bc8239e0df31ed6ea
yhONdev <yusuf7789@gmail.com>
2025-04-03 10:59:01 +0700

    change pembelian barang

--- resources/views/Purchasing/main.blade.php
+++ resources/views/Purchasing/main.blade.php
@@ -24,9 +24,9 @@
             @if($checkArea <> 0)
             <div class="row mb-2">
                 <div class="col-md-6">
-                    <button class="btn btn-outline-primary font-weight-bold onclick-submenu " data-click="dataPurchasing" id="productIn"><i class="fa-solid fa-file"></i> Data Pembelian</button>
-                    <button class="btn btn-primary font-weight-bold onclick-submenu " data-click="addPurchasing" id="pr"><i class="fa-solid fa-dolly"></i> Penerimaan Barang</button>                        
-                    <a href="{{route('returnItem')}}" class="btn btn-default font-weight-bold border-0 ml-1"><i class="fa-solid fa-rotate-left"></i> Retur Barang</a>                                         
+                    <button class="btn btn-primary font-weight-bold onclick-submenu border-0 " data-click="addPurchasing" id="pr"><i class="fa-solid fa-dolly"></i> Tambah Pembelian</button>                        
+                    <button class="btn btn-outline-primary font-weight-bold onclick-submenu border-0 " data-click="dataPurchasing" id="productIn"><i class="fa-solid fa-file"></i> List Pembelian</button>
+                    <a href="{{route('returnItem')}}" class="btn btn-default font-weight-bold border-0 ml-1"><i class="fa-solid fa-rotate-left"></i> Pengembalian Barang</a>                                         
                 </div>
                 <div class="col-md-4">
                     <select class="form-control " name="selectTransaksi" id="selectTransaksi">
