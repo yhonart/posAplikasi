62b360eb73ef7010dea7da8a04448af844bd7d4b
yhONdev <yusuf7789@gmail.com>
2025-04-05 09:20:29 +0700

    change navbar

--- resources/views/layouts/sidebarpage.blade.php
+++ resources/views/layouts/sidebarpage.blade.php
@@ -160,7 +160,7 @@
         <div class="preloader flex-column justify-content-center align-items-center">
             <img src="{{asset('public/images/loadPage.gif')}}" alt="AdminLTELogo" height="60" width="60">
           </div>
-        <nav class="main-header navbar navbar-expand navbar-purple  navbar-dark border-0">
+        <nav class="main-header navbar navbar-expand navbar-light border-0">
             <ul class="navbar-nav">
                 <li class="nav-item">
                     <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
@@ -235,6 +235,7 @@
             <img src="{{asset('public/images/favicon_dazira/favicon-32x32.png')}}" alt="AdminLTE Logo" class="brand-image" style="opacity: .8">     
                 <span class="brand-text font-weight-bold" id="ajaxClientName"></span>
             </a>
+
             <div class="sidebar">
                 <div class="user-panel mt-3 pb-3 mb-3 d-flex">
                     <div class="image">
