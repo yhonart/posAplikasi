bac87e4a2eec9d950f63bff491c6a9deb7a52c37
yhONdev <yusuf7789@gmail.com>
2025-04-07 09:40:50 +0700

    change display

--- resources/views/Cashier/cashierProductListKeyword.blade.php
+++ resources/views/Cashier/cashierProductListKeyword.blade.php
@@ -2,7 +2,7 @@
     <tbody>
         @foreach($productList as $pL)
         <tr data-id="{{$pL->idinv_stock}}">
-            <td class="p-0" colspan="3" width="20%">
+            <td class="p-0" colspan="3" width="70%">
                 {{$pL->product_name}}
             </td>
             <td class="p-0">
