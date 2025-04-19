981aa8dd09db91a81e52648a6624709cf48ae418
yhONdev <yusuf7789@gmail.com>
2025-04-19 13:37:36 +0700

    change display select category

--- resources/views/Stock/MasterData/stockFormNew.blade.php
+++ resources/views/Stock/MasterData/stockFormNew.blade.php
@@ -36,20 +36,27 @@
                 </div>
             </div>
             <div class="form-group row">
-                <label for="KodeBarang" class="form-label col-md-3">Kategori Produk <sup class="font-weight-bold text-danger">*</sup></label>
+                <label for="katProduk" class="form-label col-md-3">Produk Kategori</label>
                 <div class="col-md-6">
                     <select name="KatProduk" id="KatProduk" class="form-control form-control-sm">
-                        <option value="0" readonly>Pilih Kategori Produk</option>
+                        <option value="0">Pilih Kategori Produk</option>
                         @foreach($catProduct as $cp)
                             <option value="{{$cp->category_name}}">{{$cp->category_name}}</option>
                         @endforeach
                     </select>
-                </div>  
-                <div class="col-md-3">
-                    <button class="btn btn-info btn-sm BTN-OPEN-MODAL-GLOBAL-LG  font-weight-bold" href="{{route('M_Category')}}/AddCategory"><i class="fa-solid fa-plus"></i></button>
-                </div>             
+                </div>
             </div>
-            
+            <div class="form-group row">
+                <label for="brand" class="form-label col-md-3">Brand / Merk</label>
+                <div class="col-md-6">
+                    <select name="brand" id="brand" class="form-control form-control-sm">
+                        <option value="0" readonly>Kategori Brand</option>
+                        @foreach($manufacture as $mnf)
+                            <option value="{{$mnf->manufacture_code}}">{{$mnf->manufacture_name}}</option>
+                        @endforeach
+                    </select>
+                </div>
+            </div>
             <div class="form-group row">
                 <label for="SmallBarcode" class="form-label col-md-3">Set Minimum Stock <sup class="font-weight-bold text-danger">*</sup></label>
                 <div class="col-md-6">
