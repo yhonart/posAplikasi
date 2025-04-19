6fb38cb7668d7faf63f60eb45e4ba2a55926a0be
yhONdev <yusuf7789@gmail.com>
2025-04-19 11:34:23 +0700

    changed display

--- resources/views/Stock/MasterData/stockFormNew.blade.php
+++ resources/views/Stock/MasterData/stockFormNew.blade.php
@@ -45,9 +45,7 @@
                         @endforeach
                     </select>
                 </div>  
-                <div class="col-md-3">
-                    <button class="btn btn-info btn-sm BTN-OPEN-MODAL-GLOBAL-LG font-weight-bold" href="{{route('M_Category')}}/AddCategory"><i class="fa-solid fa-plus"></i></button>
-                </div>             
+                            
             </div>
             <div class="form-group row">
                 <label for="SmallBarcode" class="form-label col-md-3">Brand </label>
