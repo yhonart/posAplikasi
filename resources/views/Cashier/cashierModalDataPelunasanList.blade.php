0f6bce004ae0a450f34e97b3fb624c40ddc7f4e8
yhONdev <yusuf7789@gmail.com>
2025-04-20 23:05:42 +0700

    change not empty position

--- resources/views/Cashier/cashierModalDataPelunasanList.blade.php
+++ resources/views/Cashier/cashierModalDataPelunasanList.blade.php
@@ -63,14 +63,14 @@
                             @if($dP->nom_payed == $dP->nominal)
                                 {{number_format($dP->nom_payed,'0',',','.')}}
                             @elseif($dP->status == '1')   
-                                @if(empty($getLastRecord)) 
-                                    <input type="text" name="bayarPiutang" id="bayarPiutang{{$dP->idtr_kredit}}" value="" class="form-control form-control-sm form-control-border editInput nominal-bayar price-tag" autocomplete="off" onchange="saveChangePembayaran(this,'tr_kredit','nom_payed','{{$dP->idtr_kredit}}','idtr_kredit','1')" placeholder="{{number_format($dP->nom_payed,'0',',','.')}}">
-                                @else                            
-                                @foreach($getLastRecord as $glr)               
-                                    @if($glr->trx_code == $dP->from_payment_code)
+                                @if(!empty($getLastRecord)) 
+                                    @foreach($getLastRecord as $glr)               
+                                        @if($glr->trx_code == $dP->from_payment_code)
                                         <input type="text" name="bayarPiutang" id="bayarPiutang{{$dP->idtr_kredit}}" class="form-control form-control-sm form-control-border editInput nominal-bayar price-tag font-weight-bold text-danger text-right" autocomplete="off" onchange="saveChangeRecord(this,'tr_kredit_record','total_payment','{{$glr->idtr_kredit_record}}','idtr_kredit_record','1')" value="{{$glr->total_payment}}">                                    
-                                    @endif
-                                @endforeach
+                                        @endif
+                                    @endforeach
+                                @else                            
+                                    <input type="text" name="bayarPiutang" id="bayarPiutang{{$dP->idtr_kredit}}" value="" class="form-control form-control-sm form-control-border editInput nominal-bayar price-tag" autocomplete="off" onchange="saveChangePembayaran(this,'tr_kredit','nom_payed','{{$dP->idtr_kredit}}','idtr_kredit','1')" placeholder="{{number_format($dP->nom_payed,'0',',','.')}}">
                                 @endif
                             @else
                                 <input type="text" name="bayarPiutang" id="bayarPiutang{{$dP->idtr_kredit}}" value="" class="form-control form-control-sm form-control-border editInput nominal-bayar price-tag" autocomplete="off" onchange="saveChangePembayaran(this,'tr_kredit','nom_payed','{{$dP->idtr_kredit}}','idtr_kredit','1')" placeholder="{{number_format($dP->nom_payed,'0',',','.')}}">
