848dcc40d59a504b1b0ae055ea0c6c6410093db4
yhONdev <yusuf7789@gmail.com>
2025-04-20 23:03:49 +0700

    change empty

--- resources/views/Cashier/cashierModalDataPelunasanList.blade.php
+++ resources/views/Cashier/cashierModalDataPelunasanList.blade.php
@@ -63,7 +63,7 @@
                             @if($dP->nom_payed == $dP->nominal)
                                 {{number_format($dP->nom_payed,'0',',','.')}}
                             @elseif($dP->status == '1')   
-                                @if(!empty($getLastRecord)) 
+                                @if(empty($getLastRecord)) 
                                     <input type="text" name="bayarPiutang" id="bayarPiutang{{$dP->idtr_kredit}}" value="" class="form-control form-control-sm form-control-border editInput nominal-bayar price-tag" autocomplete="off" onchange="saveChangePembayaran(this,'tr_kredit','nom_payed','{{$dP->idtr_kredit}}','idtr_kredit','1')" placeholder="{{number_format($dP->nom_payed,'0',',','.')}}">
                                 @else                            
                                 @foreach($getLastRecord as $glr)               
