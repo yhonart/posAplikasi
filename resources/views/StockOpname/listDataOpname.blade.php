3de61b9acd4ef1ca82d96233e7939153bf4e6156
yhONdev <yusuf7789@gmail.com>
2025-04-25 15:27:17 +0700

    add elevation-2

--- resources/views/StockOpname/listDataOpname.blade.php
+++ resources/views/StockOpname/listDataOpname.blade.php
@@ -25,7 +25,7 @@
     </div>
     <div class="row">
         <div class="col-12">
-            <div class="card card-body text-xs">
+            <div class="card card-body text-xs elevation-2">
                 <div id="displayTableOpname"></div>
             </div>
         </div>
