eefde2c457f7a22bad3e6ce2a0d0dd76c71ece39
yhONdev <yusuf7789@gmail.com>
2025-05-03 21:54:46 +0700

    add button detail

--- resources/views/HutangCustomers/saldoKreditCustomer.blade.php
+++ resources/views/HutangCustomers/saldoKreditCustomer.blade.php
@@ -1,5 +1,9 @@
 <?php
     $no = '1';
+    $sumLimit = '0';
+    $sumNKredit = '0';
+    $sumNBayar = '0';
+    $sumSisaKredit = '0';
 ?>
 <table class="table table-sm tabl-valign-middle table-hover" id="tableKreditCustomer">
     <thead>
@@ -7,9 +11,10 @@
             <th>No.</th>
             <th>Nama Pelanggan</th>
             <th class=" bg-warning"><i class="fa-solid fa-rupiah-sign float-left"></i> <span class="float-right">Limit Kredit</span></th>
-            <th class=" bg-danger"><i class="fa-solid fa-rupiah-sign float-left"></i> <span class="float-right">Total Hutang</span></th>
-            <th class=" bg-success"><i class="fa-solid fa-rupiah-sign float-left"></i> <span class="float-right">Total Dibayar</span></th>
-            <th class=" bg-info"><i class="fa-solid fa-rupiah-sign float-left"></i> <span class="float-right">Selisih Hutang</span></th>
+            <th class=" bg-danger"><i class="fa-solid fa-rupiah-sign float-left"></i> <span class="float-right">Nilai Kredit</span></th>
+            <th class=" bg-success"><i class="fa-solid fa-rupiah-sign float-left"></i> <span class="float-right">Nilai Bayar</span></th>
+            <th class=" bg-info"><i class="fa-solid fa-rupiah-sign float-left"></i> <span class="float-right">Sisa Kredit</span></th>
+            <th></th>
         </tr>
     </thead>
     <tbody>
@@ -21,9 +26,29 @@
                 <td class="text-right bg-danger">{{number_format($ssc->nominal,'0',',','.')}}</td>
                 <td class="text-right bg-success">{{number_format($ssc->nomPayed,'0',',','.')}}</td>
                 <td class="text-right bg-info">{{number_format($ssc->saldoKredit,'0',',','.')}}</td>
+                <td>
+                    <a href="#" target="_blank" class="btn btn-sm btn-info"><i class="fa-solid fa-eye"></i></a>
+                </td>
             </tr>
-        @endforeach
+            <?php
+                $sumLimit += $ssc->kredit_limit;
+                $sumNKredit += $ssc->nominal;
+                $sumNBayar += $ssc->nomPayed;
+                $sumSisaKredit += $ssc->saldoKredit;
+            ?>
+        @endforeach        
     </tbody>
+    <tfoot class=" bg-gray-light">
+        <tr>
+            <th></th>
+            <th class="text-right">Sub Total</th>
+            <td class="text-right"></td>
+            <td class="text-right">{{number_format($sumNKredit,'0',',','.')}}</td>
+            <td class="text-right">{{number_format($sumNBayar,'0',',','.')}}</td>
+            <td class="text-right">{{number_format($sumSisaKredit,'0',',','.')}}</td>
+            <td></td>
+        </tr>
+    </tfoot>
 </table>
 <script>
     $(function(){        
