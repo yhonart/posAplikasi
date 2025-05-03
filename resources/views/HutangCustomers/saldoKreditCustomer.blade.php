af303032acc2179635d86da9785de73d65c8934d
yhONdev <yusuf7789@gmail.com>
2025-05-03 21:58:26 +0700

    remove color

--- resources/views/HutangCustomers/saldoKreditCustomer.blade.php
+++ resources/views/HutangCustomers/saldoKreditCustomer.blade.php
@@ -10,10 +10,10 @@
         <tr>
             <th>No.</th>
             <th>Nama Pelanggan</th>
-            <th class=" bg-warning"><i class="fa-solid fa-rupiah-sign float-left"></i> <span class="float-right">Limit Kredit</span></th>
-            <th class=" bg-danger"><i class="fa-solid fa-rupiah-sign float-left"></i> <span class="float-right">Nilai Kredit</span></th>
-            <th class=" bg-success"><i class="fa-solid fa-rupiah-sign float-left"></i> <span class="float-right">Nilai Bayar</span></th>
-            <th class=" bg-info"><i class="fa-solid fa-rupiah-sign float-left"></i> <span class="float-right">Sisa Kredit</span></th>
+            <th><i class="fa-solid fa-rupiah-sign float-left"></i> <span class="float-right">Limit Kredit</span></th>
+            <th><i class="fa-solid fa-rupiah-sign float-left"></i> <span class="float-right">Nilai Kredit</span></th>
+            <th><i class="fa-solid fa-rupiah-sign float-left"></i> <span class="float-right">Nilai Bayar</span></th>
+            <th><i class="fa-solid fa-rupiah-sign float-left"></i> <span class="float-right">Sisa Kredit</span></th>
             <th></th>
         </tr>
     </thead>
@@ -22,10 +22,10 @@
             <tr>
                 <td>{{$no++}}</td>
                 <td>{{$ssc->customer_store}}</td>
-                <td class="text-right bg-warning">{{number_format($ssc->kredit_limit,'0',',','.')}}</td>
-                <td class="text-right bg-danger">{{number_format($ssc->nominal,'0',',','.')}}</td>
-                <td class="text-right bg-success">{{number_format($ssc->nomPayed,'0',',','.')}}</td>
-                <td class="text-right bg-info">{{number_format($ssc->saldoKredit,'0',',','.')}}</td>
+                <td class="text-right">{{number_format($ssc->kredit_limit,'0',',','.')}}</td>
+                <td class="text-right">{{number_format($ssc->nominal,'0',',','.')}}</td>
+                <td class="text-right">{{number_format($ssc->nomPayed,'0',',','.')}}</td>
+                <td class="text-right">{{number_format($ssc->saldoKredit,'0',',','.')}}</td>
                 <td>
                     <a href="#" target="_blank" class="btn btn-sm btn-info"><i class="fa-solid fa-eye"></i></a>
                 </td>
