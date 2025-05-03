4725a7d89765f2f6f4e72fedc4fb044bd68a0bcd
yhONdev <yusuf7789@gmail.com>
2025-05-03 18:54:19 +0700

    add  fa rupiah

--- resources/views/HutangCustomers/detailPembayaran.blade.php
+++ resources/views/HutangCustomers/detailPembayaran.blade.php
@@ -54,14 +54,14 @@ $saldo = 0;
                                 @foreach($kreditRecord as $kR)
                                     <tr>
                                         <td class=" font-weight-bold">{{$kR->trx_code}}</td>
-                                        <td class=" text-right text-danger">{{number_format($kR->total_struk,'0','.',',')}}</td>
-                                        <td class=" text-right text-maroon">{{number_format($kR->saldo_kredit,'0','.',',')}}</td>
-                                        <td class=" text-right text-success">{{number_format($kR->total_payment,'0','.',',')}}</td>
+                                        <td class=" text-right text-danger"><i class="fa-solid fa-rupiah-sign"></i> {{number_format($kR->total_struk,'0','.',',')}}</td>
+                                        <td class=" text-right text-maroon"><i class="fa-solid fa-rupiah-sign"></i> {{number_format($kR->saldo_kredit,'0','.',',')}}</td>
+                                        <td class=" text-right text-success"><i class="fa-solid fa-rupiah-sign"></i> {{number_format($kR->total_payment,'0','.',',')}}</td>
                                         <td class=" text-right text-info">
                                             <?php
                                                 $saldo = $kR->saldo_kredit - $kR->total_payment;
                                             ?>
-                                            {{number_format($saldo,'0','.',',')}}
+                                            <i class="fa-solid fa-rupiah-sign"></i> {{number_format($saldo,'0','.',',')}}
                                         </td>
                                         <td>{{date('d-M-Y', strtotime($kR->date_trx))}}</td>
                                     </tr>
