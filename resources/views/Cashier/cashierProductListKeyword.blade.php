758c786a5c5933fbf7961b01f300cd5182b027f8
yhONdev <yusuf7789@gmail.com>
2025-05-05 09:35:51 +0700

    add when click

--- resources/views/Cashier/cashierProductListKeyword.blade.php
+++ resources/views/Cashier/cashierProductListKeyword.blade.php
@@ -3,7 +3,7 @@
         @foreach($productList as $pL)
         <tr data-id="{{$pL->idinv_stock}}">
             <td class="p-0" colspan="3" width="37%">
-                <a href="#" class=" font-weight-bold text-black" data-id="{{$pL->idinv_stock}}">{{$pL->product_name}}</a>                
+                <a href="#" class=" font-weight-bold text-black PRODUK-ITEM" data-idInv="{{$pL->idinv_stock}}">{{$pL->product_name}}</a>                
             </td>
             <td class="p-0" width="12%">
                 {{$pL->product_satuan}}
@@ -55,47 +55,57 @@
             } else if (event.key === 'Enter' && selectedRow >= 0) {
                 // Kirim data baris yang dipilih
                 let selectedId = rows[selectedRow].getAttribute('data-id');
-                let billNumber = "{{$billNumber}}",
-                    cusGroup = "{{$cosGroup}}",
-                    memberID = "{{$memberID}}";
-
-                let routeIndex = "{{route('Cashier')}}",
-                    urlProductList = "productList",
-                    panelProductList = $("#mainListProduct");
-                $("#tableSelectProduk").fadeOut("slow");
-                $("#disProduk").fadeIn("slow");
-                $("#fieldProduk").fadeOut("slow");
-                // $("#trInputProdut").fadeOut("slow");
-                if (selectedId) {
-                    fetch("{{route('Cashier')}}/selectResponse/" + selectedId + "/" + memberID)
-                    .then(response => response.json())
-                    .then(data => {
-                        if ((data.price) || (data.satuan) || (data.jumlah) || (data.prdStock) || (data.prodName) || (data.discount) || (data.hrgModal)) {
-                            hargaSatuan.value = accounting.formatMoney(data.price,{
-                                symbol: "",
-                                precision: 0,
-                                thousand: ".",
-                            });
-                            disProduk.value = data.prodName;
-                            formSatuan.value = data.satuan;
-                            formDisc.value = data.discount;
-                            formJumlah.value = accounting.formatMoney(data.price,{
-                                symbol: "",
-                                precision: 0,
-                                thousand: ".",
-                            });
-                            formStock.value = data.prdStock;
-                            hargaBeli.value = data.hrgModal;                            
-                            disStockAwal.value = data.prdStock;                            
-                        }
-                    }); 
-                    $("#formQty").val("1").focus().select();                   
-                    $("#fieldProduk").val(null);
-                    $("#stockID").val(selectedId);
-                    $("#cusGroup").val(cusGroup);
-                }
+                functEnter(selectedId);
             }
         });
+
+        $('.PRODUK-ITEM').on('click', function (e) {
+            e.preventDefault();
+            let selectedId = $(this).attr('data-idInv');
+            functEnter(selectedId);
+        });
+
+        function functEnter(selectedId){
+            let billNumber = "{{$billNumber}}",
+                cusGroup = "{{$cosGroup}}",
+                memberID = "{{$memberID}}";
+
+            let routeIndex = "{{route('Cashier')}}",
+                urlProductList = "productList",
+                panelProductList = $("#mainListProduct");
+            $("#tableSelectProduk").fadeOut("slow");
+            $("#disProduk").fadeIn("slow");
+            $("#fieldProduk").fadeOut("slow");
+            // $("#trInputProdut").fadeOut("slow");
+            if (selectedId) {
+                fetch("{{route('Cashier')}}/selectResponse/" + selectedId + "/" + memberID)
+                .then(response => response.json())
+                .then(data => {
+                    if ((data.price) || (data.satuan) || (data.jumlah) || (data.prdStock) || (data.prodName) || (data.discount) || (data.hrgModal)) {
+                        hargaSatuan.value = accounting.formatMoney(data.price,{
+                            symbol: "",
+                            precision: 0,
+                            thousand: ".",
+                        });
+                        disProduk.value = data.prodName;
+                        formSatuan.value = data.satuan;
+                        formDisc.value = data.discount;
+                        formJumlah.value = accounting.formatMoney(data.price,{
+                            symbol: "",
+                            precision: 0,
+                            thousand: ".",
+                        });
+                        formStock.value = data.prdStock;
+                        hargaBeli.value = data.hrgModal;                            
+                        disStockAwal.value = data.prdStock;                            
+                    }
+                }); 
+                $("#formQty").val("1").focus().select();                   
+                $("#fieldProduk").val(null);
+                $("#stockID").val(selectedId);
+                $("#cusGroup").val(cusGroup);
+            }
+        }
         // CSS untuk menandai baris yang dipilih
         const style = document.createElement('style');
         style.innerHTML = `
