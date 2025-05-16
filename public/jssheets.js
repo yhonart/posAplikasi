f939510adae34971d733bda71f813c983f86765a
yhONdev <yusuf7789@gmail.com>
2025-05-16 10:38:32 +0700

    add window

--- public/jssheets.js
+++ public/jssheets.js
@@ -1,5 +1,4 @@
 import { highcharts } from "/dazbuild/plugins/highcharts/code/highcharts.js";
 import { highchartsmore } from "/dazbuild/plugins/highcharts/code/highcharts-more.js";
 
-highcharts();
-highchartsmore();
+window.$ = highcharts, highchartsmore;
