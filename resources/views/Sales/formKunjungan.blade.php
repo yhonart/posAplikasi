0ab72001a764480e8cf5efbf4d53da3e215a0210
yhONdev <yusuf7789@gmail.com>
2025-06-02 05:34:22 +0700

    add icon and display file

--- resources/views/Sales/formKunjungan.blade.php
+++ resources/views/Sales/formKunjungan.blade.php
@@ -38,29 +38,29 @@
             </div>
             <hr>
             <div class="form-group">
-                <button type="button" class="btn btn-sm btn-danger" onclick="getLocation()">Get Location</button>
+                <button type="button" class="btn btn-sm btn-danger" onclick="getLocation()"><i class="fa-solid fa-map-location"></i> Get Location</button>
             </div>
             <div class="form-group row">
                 <label for="Latitude" class="col-md-4">Latitude</label>
                 <div class="col-md-4">
-                    <input type="text" class="form-control form-control-sm" name="Latitude" id="Latitude">
+                    <input type="text" class="form-control form-control-sm" name="Latitude" id="Latitude" readonly>
                 </div>
             </div>
             <div class="form-group row">
                 <label for="Longitude" class="col-md-4">Longitude</label>
                 <div class="col-md-4">
-                    <input type="text" class="form-control form-control-sm" name="Longitude" id="Longitude">
+                    <input type="text" class="form-control form-control-sm" name="Longitude" id="Longitude" readonly>
                 </div>
             </div>
             <hr>
             <div class="form-group row">
                 <label for="dateFU" class="col-md-4">Foto Toko</label>
                 <div class="col-md-4">
-                    <input type="file" class="form-control form-control-file" name="dateFU" id="dateFU">
+                    <input type="file" class="form-control-file" name="dateFU" id="dateFU">
                 </div>
             </div>
             <div class="form-group">
-                <button type="submit" class="btn btn-success font-weight-bold" id="btnSaveKunjungan">Simpan</button>
+                <button type="submit" class="btn btn-success font-weight-bold" id="btnSaveKunjungan"><i class="fa-solid fa-floppy-disk"></i> Simpan</button>
             </div>
         </form>
     </div>
