ad73e18d502f96e931e99d8bdfd1f473d6e4145c
yhONdev <yusuf7789@gmail.com>
2025-06-01 22:53:13 +0700

    add row

--- resources/views/Sales/mainSales.blade.php
+++ resources/views/Sales/mainSales.blade.php
@@ -10,16 +10,20 @@
         <div class="row">
             <div class="col-md-8">
                 <div class="card card-body">
-                    <a href="#" class="btn btn-app bg-info">
-                        <span class="badge bg-info">0</span>
-                        <i class="fa-solid fa-table-list"></i> Daftar Kunjungan
-                    </a>
-                    <a href="{{route('sales')}}/formKunjungan" class="btn btn-app bg-info">                    
-                        <i class="fa-solid fa-file"></i> Input Kunjungan
-                    </a>
-                    <a href="#" class="btn btn-app bg-info">                    
-                        <i class="fa-solid fa-chart-line"></i> Sales Dashboard
-                    </a>                    
+                    <div class="row">
+                        <div class="col-md-12">
+                            <a href="#" class="btn btn-app bg-info">
+                                <span class="badge bg-info">0</span>
+                                <i class="fa-solid fa-table-list"></i> Daftar Kunjungan
+                            </a>
+                            <a href="{{route('sales')}}/formKunjungan" class="btn btn-app bg-info">                    
+                                <i class="fa-solid fa-file"></i> Input Kunjungan
+                            </a>
+                            <a href="#" class="btn btn-app bg-info">                    
+                                <i class="fa-solid fa-chart-line"></i> Sales Dashboard
+                            </a>
+                        </div>
+                    </div>
                 </div>
                 <div class="card card-body border-0 shadow">
                     <div id="displaySales"></div>
