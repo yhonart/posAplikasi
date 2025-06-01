e21a81bc4295571da58e2d7afd61f93f0bb02914
yhONdev <yusuf7789@gmail.com>
2025-06-01 22:52:04 +0700

    change layout sales

--- app/Http/Controllers/SalesController.php
+++ app/Http/Controllers/SalesController.php
@@ -3,8 +3,25 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Carbon;
 
 class SalesController extends Controller
 {
-    //
+    public function main(){
+
+    }
+
+    public function daftarKunjungan (){
+
+    }
+
+    public function formKunjungan (){
+        return view('Sales/formKunjungan');
+    }
+
+    public function salesDasboard (){
+        
+    }
 }
--- resources/views/Sales/mainSales.blade.php
+++ resources/views/Sales/mainSales.blade.php
@@ -9,19 +9,27 @@
         </div>
         <div class="row">
             <div class="col-md-8">
-                <a href="#" class="btn btn-app bg-info">
-                    <span class="badge bg-info">0</span>
-                    <i class="fa-solid fa-table-list"></i> Daftar Kunjungan
-                </a>
-                <a href="#" class="btn btn-app bg-info">                    
-                    <i class="fa-solid fa-file"></i> Input Kunjungan
-                </a>
-                <a href="#" class="btn btn-app bg-info">                    
-                    <i class="fa-solid fa-chart-line"></i> Sales Dashboard
-                </a>
+                <div class="card card-body">
+                    <a href="#" class="btn btn-app bg-info">
+                        <span class="badge bg-info">0</span>
+                        <i class="fa-solid fa-table-list"></i> Daftar Kunjungan
+                    </a>
+                    <a href="{{route('sales')}}/formKunjungan" class="btn btn-app bg-info">                    
+                        <i class="fa-solid fa-file"></i> Input Kunjungan
+                    </a>
+                    <a href="#" class="btn btn-app bg-info">                    
+                        <i class="fa-solid fa-chart-line"></i> Sales Dashboard
+                    </a>                    
+                </div>
+                <div class="card card-body border-0 shadow">
+                    <div id="displaySales"></div>
+                </div>
             </div>
             <div class="col-md-4">
-                <div class="card">
+                <div class="card border-0 shadow">
+                    <div class="card-header">
+                        <h3 class="card-title">Update Data Terbaru</h3>
+                    </div>
                     <div class="card-body">
 
                     </div>
