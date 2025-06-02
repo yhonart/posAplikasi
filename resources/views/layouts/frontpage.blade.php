62b1735f42fc6ae47565da6ef770418881b8e09f
yhONdev <yusuf7789@gmail.com>
2025-06-02 22:40:41 +0700

    move position form logout

--- resources/views/layouts/frontpage.blade.php
+++ resources/views/layouts/frontpage.blade.php
@@ -167,19 +167,17 @@
                           <i class="fa-solid fa-user"></i> | {{Auth::user()->name}}
                         </a>
                         <div class="dropdown-menu dropdown-menu-lg dropdown-menu-right">
-                            <a class="dropdown-item" href="{{ route('logout') }}"
-                                onclick="event.preventDefault();
-                                document.getElementById('logout-form').submit();">
+                            <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
                                 <i class="fa-solid fa-right-from-bracket"></i> {{ __('Logout') }}
-                            </a>
-                            <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                                @csrf
-                            </form> 
+                            </a>                            
                         </div>
                      </li>
                 </ul>
             </div>
         </nav>
+        <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+            @csrf
+        </form> 
         
         <div class="content-wrapper bg-white">
             @yield('content')
