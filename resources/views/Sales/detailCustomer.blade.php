be9554cae0cb8abce53bcdf6edd28ff6e33f3559
yhONdev <yusuf7789@gmail.com>
2025-06-02 14:32:27 +0700

    change latitude

--- resources/views/Sales/detailCustomer.blade.php
+++ resources/views/Sales/detailCustomer.blade.php
@@ -30,21 +30,24 @@
             </div>
         </div>
 </div>
-
+<script>(g=>{var h,a,k,p="The Google Maps JavaScript API",c="google",l="importLibrary",q="__ib__",m=document,b=window;b=b[c]||(b[c]={});var d=b.maps||(b.maps={}),r=new Set,e=new URLSearchParams,u=()=>h||(h=new Promise(async(f,n)=>{await (a=m.createElement("script"));e.set("libraries",[...r]+"");for(k in g)e.set(k.replace(/[A-Z]/g,t=>"_"+t[0].toLowerCase()),g[k]);e.set("callback",c+".maps."+q);a.src=`https://maps.${c}apis.com/maps/api/js?`+e;d[q]=f;a.onerror=()=>h=n(Error(p+" could not load."));a.nonce=m.querySelector("script[nonce]")?.nonce||"";m.head.append(a)}));d[l]?console.warn(p+" only loads once. Ignoring:",g):d[l]=(f,...n)=>r.add(f)&&u().then(()=>d[l](f,...n))})
+        ({key: "AIzaSyBWATSqbKPi6KunkVey74s45OojCu6Ws04", v: "weekly"});</script>
 <script>
-    let map;
     let latVal = {{$detailCus->latitude}};
     let longVal = {{$detailCus->longtitude}};
 
-    function initMap() {
-        // Longitude dan Latitude yang ingin Anda tampilkan
-        const lokasiSaya = { lat: latVal, lng: longVal }; 
-        map = new google.maps.Map(document.getElementById("map"), {
-            center: lokasiSaya, // Posisikan peta di lokasi yang ditentukan
-            zoom: 20,          // Atur level zoom (semakin tinggi, semakin dekat)
-        });       
-    }   
+    let map;
+
+    async function initMap() {
+    const { Map } = await google.maps.importLibrary("maps");
+
+    map = new Map(document.getElementById("map"), {
+        center: { lat: latVal, lng: longVal },
+        zoom: 8,
+    });
+    }
+    initMap();
 </script>
-<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBWATSqbKPi6KunkVey74s45OojCu6Ws04&callback=initMap&loading=async"></script>
+
 
 @endsection
