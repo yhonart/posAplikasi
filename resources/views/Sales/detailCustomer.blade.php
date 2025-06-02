534f971767f7a874ae86038116fce041b6db143f
yhONdev <yusuf7789@gmail.com>
2025-06-02 14:06:50 +0700

    change code latitude

--- resources/views/Sales/detailCustomer.blade.php
+++ resources/views/Sales/detailCustomer.blade.php
@@ -11,18 +11,27 @@
         <div class="row">
             <div class="col-md-12">
                 <div id="map"></div>
-                <span>Text</span>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-12">
+                <div class="card card-body shadow border-0">
+                    <dl class="row">
+                        <dt class="col-md-4">Nama Toko</dt>
+                        <dd class="col-md-4">{{$detailCus->store_name}}</dd>
+                    </dl>
+                </div>
             </div>
         </div>
 </div>
 
 <script>
     let map;
+    let latVal = "{{$detailCus->latitude}}";
+    let longVal = "{{$detailCus->longtitude}}";
     function initMap() {
         // Longitude dan Latitude yang ingin Anda tampilkan
-        const lokasiSaya = { lat: -6.1753924, lng: 106.8271528 }; // Contoh: Monas, Jakarta
-
-        // Buat objek peta baru
+        const lokasiSaya = { lat: latVal, lng: longVal }; 
         map = new google.maps.Map(document.getElementById("map"), {
             center: lokasiSaya, // Posisikan peta di lokasi yang ditentukan
             zoom: 15,          // Atur level zoom (semakin tinggi, semakin dekat)
