9970aa624863a1c845c451cd03ca18bd12d4d954
yhONdev <yusuf7789@gmail.com>
2025-06-02 15:40:52 +0700

    change long

--- resources/views/Sales/detailCustomer.blade.php
+++ resources/views/Sales/detailCustomer.blade.php
@@ -47,7 +47,7 @@
     function initMap() {
     const mapOptions = {
         zoom: 8,
-        center: { lat: -34.397, lng: 150.644 },
+        center: { lat: latVal, lng: longVal },
     };
 
     map = new google.maps.Map(document.getElementById("map"), mapOptions);
