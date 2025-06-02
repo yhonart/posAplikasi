e79e42387a3a989ac02a399d0e78ab77d6b6d3cf
yhONdev <yusuf7789@gmail.com>
2025-06-02 11:43:01 +0700

    change script

--- resources/views/Sales/detailCustomer.blade.php
+++ resources/views/Sales/detailCustomer.blade.php
@@ -21,35 +21,33 @@
      * SPDX-License-Identifier: Apache-2.0
      */
     async function initMap() {
-    // Request needed libraries.
-    let latVal = "{{$detailCus->latitude}}";
-    let longVal = "{{$detailCus->longtitude}}";
-    const { Map } = await google.maps.importLibrary("maps");
-    const myLatlng = { lat: latVal, lng: longVal };
-    const map = new google.maps.Map(document.getElementById("map"), {
-        zoom: 20,
-        center: myLatlng,
-    });
-    // Create the initial InfoWindow.
-    let infoWindow = new google.maps.InfoWindow({
-        content: "Click the map to get Lat/Lng!",
-        position: myLatlng,
-    });
-
-    infoWindow.open(map);
-    // Configure the click listener.
-    map.addListener("click", (mapsMouseEvent) => {
-        // Close the current InfoWindow.
-        infoWindow.close();
-        // Create a new InfoWindow.
-        infoWindow = new google.maps.InfoWindow({
-        position: mapsMouseEvent.latLng,
+        // Request needed libraries.
+        const { Map } = await google.maps.importLibrary("maps");
+        const myLatlng = { lat: -25.363, lng: 131.044 };
+        const map = new google.maps.Map(document.getElementById("map"), {
+            zoom: 4,
+            center: myLatlng,
         });
-        infoWindow.setContent(
-        JSON.stringify(mapsMouseEvent.latLng.toJSON(), null, 2),
-        );
+        // Create the initial InfoWindow.
+        let infoWindow = new google.maps.InfoWindow({
+            content: "Click the map to get Lat/Lng!",
+            position: myLatlng,
+        });
+
         infoWindow.open(map);
-    });
+        // Configure the click listener.
+        map.addListener("click", (mapsMouseEvent) => {
+            // Close the current InfoWindow.
+            infoWindow.close();
+            // Create a new InfoWindow.
+            infoWindow = new google.maps.InfoWindow({
+            position: mapsMouseEvent.latLng,
+            });
+            infoWindow.setContent(
+            JSON.stringify(mapsMouseEvent.latLng.toJSON(), null, 2),
+            );
+            infoWindow.open(map);
+        });
     }
 
     initMap();
