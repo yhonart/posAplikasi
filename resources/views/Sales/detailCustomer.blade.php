4522a70ff557d63ff4e147401e1013f672915800
yhONdev <yusuf7789@gmail.com>
2025-06-02 13:56:13 +0700

    change key project

--- resources/views/Sales/detailCustomer.blade.php
+++ resources/views/Sales/detailCustomer.blade.php
@@ -16,19 +16,26 @@
 </div>
 
 <script>
+    /**
+     * @license
+     * Copyright 2019 Google LLC. All Rights Reserved.
+     * SPDX-License-Identifier: Apache-2.0
+     */
     let map;
+    function initMap() {
+        // Longitude dan Latitude yang ingin Anda tampilkan
+        const lokasiSaya = { lat: -6.1753924, lng: 106.8271528 }; // Contoh: Monas, Jakarta
 
-async function initMap() {
-    const { Map } = await google.maps.importLibrary("maps");
+        // Buat objek peta baru
+        map = new google.maps.Map(document.getElementById("map"), {
+            center: lokasiSaya, // Posisikan peta di lokasi yang ditentukan
+            zoom: 15,          // Atur level zoom (semakin tinggi, semakin dekat)
+        });
 
-    map = new Map(document.getElementById("map"), {
-        center: { lat: -34.397, lng: 150.644 },
-        zoom: 8,
-    });
-    }
-    initMap();
+        // Tambahkan marker ke peta
+        
+    }   
 </script>
-<script>(g=>{var h,a,k,p="The Google Maps JavaScript API",c="google",l="importLibrary",q="__ib__",m=document,b=window;b=b[c]||(b[c]={});var d=b.maps||(b.maps={}),r=new Set,e=new URLSearchParams,u=()=>h||(h=new Promise(async(f,n)=>{await (a=m.createElement("script"));e.set("libraries",[...r]+"");for(k in g)e.set(k.replace(/[A-Z]/g,t=>"_"+t[0].toLowerCase()),g[k]);e.set("callback",c+".maps."+q);a.src=`https://maps.${c}apis.com/maps/api/js?`+e;d[q]=f;a.onerror=()=>h=n(Error(p+" could not load."));a.nonce=m.querySelector("script[nonce]")?.nonce||"";m.head.append(a)}));d[l]?console.warn(p+" only loads once. Ignoring:",g):d[l]=(f,...n)=>r.add(f)&&u().then(()=>d[l](f,...n))})
-        ({key: "AIzaSyB41DRUbKWJHPxaFjMAwdrzWzbVKartNGg", v: "weekly"});</script>
+<script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBWATSqbKPi6KunkVey74s45OojCu6Ws04&callback=initMap"></script>
 
 @endsection
