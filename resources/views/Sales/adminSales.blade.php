120235e721f38d44de677fb4642eb4ee3197df2e
yhONdev <yusuf7789@gmail.com>
2025-06-04 15:43:12 +0700

    change admin menu

--- resources/views/Sales/adminSales.blade.php
+++ resources/views/Sales/adminSales.blade.php
@@ -4,7 +4,27 @@
     <div class="container-fluid">
         <div class="row">
             <div class="col-md-12">
-
+                <nav class="navbar navbar-expand navbar-light">
+                    <button type="button" class="navbar-toggler" data-toggler="collapse" data-target="#navbarSupportAdmin" aria-controls="navbarSupportAdmin" aria-expanded="false" aria-label="Toggle Navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse order-3" id="navbarSupportAdmin">
+                        <ul class="navbar-nav">
+                            <li class="nav-item">
+                                <a href="#" class="nav-link" data-link="cstProduct">Produk</a>
+                            </li>
+                            <li class="nav-item">
+                                <a href="#" class="nav-link" data-link="cstCustomer">Pelanggan</a>
+                            </li>
+                            <li class="nav-item">
+                                <a href="#" class="nav-link" data-link="salesOrder">Sales Order</a>
+                            </li>
+                            <li class="nav-item">
+                                <a href="#" class="nav-link" data-link="logistic">Logistik</a>
+                            </li>
+                        </ul>
+                    </div>
+                </nav>
             </div>
         </div>
     </div>        
