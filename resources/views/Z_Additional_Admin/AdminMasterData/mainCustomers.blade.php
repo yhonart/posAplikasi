738c697f2a0f19733c7f25a9b2883f91a4afc072
yhONdev <yusuf7789@gmail.com>
2025-06-05 23:10:04 +0700

    add button create customer

--- resources/views/Z_Additional_Admin/AdminMasterData/mainCustomers.blade.php
+++ resources/views/Z_Additional_Admin/AdminMasterData/mainCustomers.blade.php
@@ -4,8 +4,14 @@
             <div class="card-header">
                 <h3 class="card-title">Customer Data</h3>
             </div>
-            <div class="card-body">
-                <span>Test</span>
+            <div class="card-body table-responsive">
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="btn-group">
+                            <button type="button" class="btn btn-primary font-weight-bold BTN-OPEN-MODAL-GLOBAL-LG" href="{{route('Customers')}}/AddCustomers" ><i class="fa-solid fa-address-book"></i> Tambah</button>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
