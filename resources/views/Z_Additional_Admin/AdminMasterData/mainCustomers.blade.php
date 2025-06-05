2babd451b0563102c47727753f4b08a2505b8376
yhONdev <yusuf7789@gmail.com>
2025-06-05 23:07:41 +0700

    change main customers

--- resources/views/Z_Additional_Admin/AdminMasterData/mainCustomers.blade.php
+++ resources/views/Z_Additional_Admin/AdminMasterData/mainCustomers.blade.php
@@ -1,12 +1,12 @@
-@extends('layouts.frontpage')
-@section('content')
-<div class="content-header">
-    <div class="container-fluid">
-    <div class="row mb-2">
-        <div class="col-sm-6">
-        <h1 class="m-0">Customer Data</h1>
-        </div><!-- /.col -->
-    </div><!-- /.row -->
-    </div><!-- /.container-fluid -->
+<div class="row">
+    <div class="col-md-12">
+        <div class="card">
+            <div class="card-header">
+                <h3 class="card-title">Customer Data</h3>
+            </div>
+            <div class="card-body">
+                <span>Test</span>
+            </div>
+        </div>
+    </div>
 </div>
-@endsection
