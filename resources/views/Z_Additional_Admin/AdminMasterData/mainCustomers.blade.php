5193b659a775b843815358504c405e41413fcb47
yhONdev <yusuf7789@gmail.com>
2025-06-06 06:29:20 +0700

    change border data pelanggan

--- resources/views/Z_Additional_Admin/AdminMasterData/mainCustomers.blade.php
+++ resources/views/Z_Additional_Admin/AdminMasterData/mainCustomers.blade.php
@@ -1,22 +1,24 @@
 <div class="row">
     <div class="col-md-12">
-        <div class="row">
+        <div class="row mb-2">
             <div class="col-md-4">
                 <div class="btn-group">
                     <button type="button" class="btn btn-sm btn-primary font-weight-bold BTN-OPEN-MODAL-GLOBAL-LG" href="{{route('Customers')}}/AddCustomers" ><i class="fa-solid fa-address-book"></i> New</button>
                     <a href="{{route('Customers')}}/downloadAllCustomer" class="btn btn-sm btn-success font-weight-bold" target="_blank"><i class="fa-solid fa-file-excel"></i> Download</a>
                 </div>
             </div>
-            <div class="col-md-4">
-                
-            </div>
         </div>
         <div class="row">
             <div class="col-12 col-md-3">                
-                <div class="card card-body text-xs p-0 table-responsive " style="height:700px;">
-                    <input type="text" name="searchCustomer" id="searchCustomer" class="form-control mb-1 " placeholder="Cari nama pelanggan" autofocus>
-                    @include('Global.global_spinner')
-                    <div id="displayTableCustomers"></div>
+                <div class="card text-xs p-0 table-responsive border border-2 border-info shadow" style="height:700px;">
+                    <div class="card-header">
+                        <h3 class="card-title">Nama Pelanggan</h3>
+                    </div>
+                    <div class="card-body">
+                        <input type="text" name="searchCustomer" id="searchCustomer" class="form-control mb-1 " placeholder="Cari nama pelanggan" autofocus>
+                        @include('Global.global_spinner')
+                        <div id="displayTableCustomers"></div>
+                    </div>
                 </div>
             </div>
             <div class="col-12 col-md-9">
