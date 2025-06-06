f4ac965dde1eee40b94aa974885231010a0e7841
yhONdev <yusuf7789@gmail.com>
2025-06-06 11:35:32 +0700

    add card into modal new category

--- resources/views/Z_Additional_Admin/AdminMasterData/mainCategoryNew.blade.php
+++ resources/views/Z_Additional_Admin/AdminMasterData/mainCategoryNew.blade.php
@@ -1,25 +1,32 @@
 <div class="row d-flex justify-content-center">
     <div class="col-md-4">
-        <form id="formNewCategory">
-            <div class="form-group row">
-                <label for="categoryCode" class="form-label col-md-3">Kode Kategory</label>
-                <div class="col-md-4">
-                    <input type="text" class="form-control form-control-sm" name="categoryCode" id="categoryCode">
-                </div>
+        <div class="card">
+            <div class="card-header">
+                <h3 class="card-title">Tambah Kategori</h3>
             </div>
-            <div class="form-group row">
-                <label for="categoryName" class="form-label col-md-3">Nama Kategory</label>
-                <div class="col-md-4">
-                    <input type="text" class="form-control form-control-sm" name="categoryName" id="categoryName">
-                </div>
+            <div class="card-body">
+                <form id="formNewCategory">
+                    <div class="form-group row">
+                        <label for="categoryCode" class="form-label col-md-3">Kode Kategory</label>
+                        <div class="col-md-4">
+                            <input type="text" class="form-control form-control-sm" name="categoryCode" id="categoryCode">
+                        </div>
+                    </div>
+                    <div class="form-group row">
+                        <label for="categoryName" class="form-label col-md-3">Nama Kategory</label>
+                        <div class="col-md-4">
+                            <input type="text" class="form-control form-control-sm" name="categoryName" id="categoryName">
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <button type="submit" class="btn btn-sm btn-success" id="saveCategory">Simpan</button>
+                    </div>
+                </form>
             </div>
-            <div class="form-group">
-                <button type="submit" class="btn btn-sm btn-success" id="saveCategory">Simpan</button>
-            </div>
-        </form>
+        </div>
     </div>
 </div>
 
 <script>
-    
+
 </script>
