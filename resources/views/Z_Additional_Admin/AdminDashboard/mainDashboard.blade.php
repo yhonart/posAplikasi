315ce62decff54545088f474a8e2cc65e44b8331
yhONdev <yusuf7789@gmail.com>
2025-06-08 20:41:31 +0700

    add count

--- resources/views/Z_Additional_Admin/AdminDashboard/mainDashboard.blade.php
+++ resources/views/Z_Additional_Admin/AdminDashboard/mainDashboard.blade.php
@@ -1,10 +1,33 @@
-<div class="row">
+<div class="row d-block d-md-none">
     <div class="col-md-12">
         <div>
             <a href="#" class="btn btn-app bg-secondary">
                 <i class="fas fa-barcode"></i>
                 Product
             </a>
+        </div>
+    </div>
+</div>
+
+<div class="row">
+    <div class="col-md-9">
+        <div class="card border border-info">
+            <div class="card-body">
+                <div class="row">
+                    <div class="col-md-3 col-6">
+                        <div class="info-box">
+                            <span class="info-box-icon bg-info elevation-1"><i class="fas fa-boxes-stacked"></i></span>
+                            <div class="info-box-content">
+                                <span class="info-box-text">All Product</span>
+                                <span class="info-box-number">
+                                    {{$productCode}}
+                                    <a href="#">More Info <i class="fa-solid fa-arrow-right"></i></a>
+                                </span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 </div>
