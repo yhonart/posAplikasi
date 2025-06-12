afb8e4c17787ffe9ba0cedd329d4259eaf4ddca3
yhONdev <yusuf7789@gmail.com>
2025-06-12 16:40:31 +0700

    add product category

--- resources/views/Z_Additional_Admin/AdminInventory/mainInventoryTable.blade.php
+++ resources/views/Z_Additional_Admin/AdminInventory/mainInventoryTable.blade.php
@@ -7,9 +7,10 @@
         <table class="table table-sm table-valign-middle table-striped table-hover">
             <thead>
                 <tr>
-                    <th>No. {{$companyID}}</th>
+                    <th>No.</th>
                     <th>Product Code</th>
                     <th>Product Name</th>
+                    <th>Category</th>
                     <th>Saldo</th>
                     <th>#</th>
                 </tr>
@@ -17,9 +18,10 @@
             <tbody>
                 @foreach($docInventory as $di)
                     <tr>
-                        <td>{{$no++}}</td>
-                        <td>{{$di->product_code}}</td>
+                        <td class="text-center">{{$no++}}</td>
+                        <td class="font-weight-bold text-info">{{$di->product_code}}</td>
                         <td>{{$di->product_name}}</td>
+                        <td>{{$di->product_category}}</td>
                         <td>{{$di->stock}}</td>
                         <td>
                             <button type="button" class="btn btn-info btn-sm BTN-DETAIL"><i class="fa-solid fa-magnifying-glass"></i></button>
