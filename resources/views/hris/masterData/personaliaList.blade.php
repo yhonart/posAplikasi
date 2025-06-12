79593833d2016494a534be63d41d53b58084617b
yhONdev <yusuf7789@gmail.com>
2025-06-12 08:54:28 +0700

    add php echo

--- resources/views/hris/masterData/personaliaList.blade.php
+++ resources/views/hris/masterData/personaliaList.blade.php
@@ -32,7 +32,7 @@
                             <td>{{$u->username}}</td>
                             <td>{{$u->site_name}}</td>
                             <td>{{$hakAkses[$u->hakakses]}}</td>
-                            <td>[{{$u->company_code}}] $u->company_name</td>
+                            <td>[{{$u->company_code}}] {{$u->company_name}}</td>
                             <td class="text-right">                                
                                 <a class="btn btn-sm  btn-info BTN-OPEN-MODAL-GLOBAL-LG font-weight-bold" href="{{route('Personalia')}}/modalHakAkses/{{$u->id}}"><i class="fa-solid fa-shield-halved"></i> Hak Akses</a>
                                 <a class="btn btn-sm  btn-danger font-weight-bold DEL-PERSONALIA" href="#" data-id="{{$u->id}}"><i class="fa-solid fa-trash"></i> Delete</a>
