fc818cd8d2f07b25fa992accb85fbd08442e878a
yhONdev <yusuf7789@gmail.com>
2025-07-03 08:52:16 +0700

    change menu stock opname

--- resources/views/Z_Additional_Admin/AdminInventory/mainStockOpname.blade.php
+++ resources/views/Z_Additional_Admin/AdminInventory/mainStockOpname.blade.php
@@ -1,4 +1,12 @@
 <section class="container">
+    <div class="row mb-2">
+        <div class="col-md-12">
+            <div class=" btn-group">
+                <button class=" btn btn-default btn-sm border-0 font-weight-bold OPNAME-MENU" data-path="mainTableStockOpname">List Data Opname</button>
+                <button class=" btn btn-default btn-sm border-0 font-weight-bold OPNAME-MENU" data-path="displayStockOpname">Input Dok.Stock Opname</button>
+            </div>
+        </div>
+    </div>
     <div class="row">
         <div class="col-md-12">
             <div id="displayStockOpname"></div>
@@ -9,5 +17,16 @@
 <script>
     $(function(){
         $("#displayStockOpname").load("{{route('sales')}}/displayStockOpname");
-    })
+    });
+
+    $(document).ready(function() {
+        $('.ITEM-MAIN-MENU').on('click', function (e) {
+            e.preventDefault();
+            let path = $(this).attr('data-path');
+
+            $("#displayStockOpname").load("{{route('sales')}}/"+path);
+        });
+    
+    });
+
 </script>
