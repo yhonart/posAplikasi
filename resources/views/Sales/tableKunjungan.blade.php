914f07570e448c7cbef293e83cf140ff969f510e
yhONdev <yusuf7789@gmail.com>
2025-07-04 11:21:50 +0700

    change bg gradient purple

--- resources/views/Sales/tableKunjungan.blade.php
+++ resources/views/Sales/tableKunjungan.blade.php
@@ -9,8 +9,8 @@
 ?>
 <div class="row">
     <div class="col-md-12">
-        <div class="card text-xs card-purple">
-            <div class="card-header">
+        <div class="card text-xs">
+            <div class="card-header bg-gradient-purple">
                 <h3 class="card-title font-weight-bold">Daftar Kunjungan</h3>
             </div>
             <div class="card-body">
