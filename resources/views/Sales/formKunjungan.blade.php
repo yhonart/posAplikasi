6dae61ff20cdda795a307d7611eeb7c77de3d9be
yhONdev <yusuf7789@gmail.com>
2025-07-06 20:59:41 +0700

    change alert

--- resources/views/Sales/formKunjungan.blade.php
+++ resources/views/Sales/formKunjungan.blade.php
@@ -164,12 +164,12 @@
         }
     }
 
-    $('.multi-field-wrapper').each(function() { 
-        alert("OK");
+    $('.multi-field-wrapper').each(function() {         
         var $wrapper = $('.multi-fields', this);
         $(".add-field", $(this)).click(function(e) {
             // $('.multi-field:first-child', $wrapper).clone(true).appendTo($wrapper).find('#produk').val('').focus();
             $('.multi-field:first-child', $wrapper).clone(true).appendTo($wrapper);
+            alert("OK");
         });
         $('.multi-field .remove-field', $wrapper).click(function() {
             if ($('.multi-field', $wrapper).length > 1)
