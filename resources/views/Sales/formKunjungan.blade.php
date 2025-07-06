45c839d1ad4fe84117a8d4e9528344dab20957e9
yhONdev <yusuf7789@gmail.com>
2025-07-06 20:15:49 +0700

    add function e

--- resources/views/Sales/formKunjungan.blade.php
+++ resources/views/Sales/formKunjungan.blade.php
@@ -157,7 +157,8 @@
         }
     }
 
-    $('.multi-field-wrapper').each(function() {
+    $('.multi-field-wrapper').each(function(e) {
+        e.preventDefault();
 	    var $wrapper = $('.multi-fields', this);
 	    $(".add-field", $(this)).click(function(e) {
 	        $('.multi-field:first-child', $wrapper).clone(true).appendTo($wrapper).find('#produk').val('').focus();
