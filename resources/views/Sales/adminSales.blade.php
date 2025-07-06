2e9aaec6c2752edc64bec99f11d98c9efce6258e
yhONdev <yusuf7789@gmail.com>
2025-07-06 20:35:03 +0700

    change add multifield

--- resources/views/Sales/adminSales.blade.php
+++ resources/views/Sales/adminSales.blade.php
@@ -98,6 +98,7 @@
     $(function(){
         $("#divContent").load(route_main+'/mainDashboard');
     });
+    
     $(document).ready(function() {
         $.ajaxSetup({
             headers: {
@@ -124,6 +125,18 @@
         });
         el_modal_all.on('hidden.bs.modal', function () {
             $(this).find(id_modal_content).html('');
+        });
+    });
+
+    $('.multi-field-wrapper').each(function() {        
+        var $wrapper = $('.multi-fields', this);
+        $(".add-field", $(this)).click(function(e) {
+            // $('.multi-field:first-child', $wrapper).clone(true).appendTo($wrapper).find('#produk').val('').focus();
+            $('.multi-field:first-child', $wrapper).clone(true).appendTo($wrapper);
+        });
+        $('.multi-field .remove-field', $wrapper).click(function() {
+            if ($('.multi-field', $wrapper).length > 1)
+                $(this).parent('.multi-field').remove();
         });
     });
 </script>
