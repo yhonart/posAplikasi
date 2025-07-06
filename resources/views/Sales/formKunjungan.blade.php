ef15c4d2bada0ff58989535904b84d26f8cf1400
yhONdev <yusuf7789@gmail.com>
2025-07-06 21:01:14 +0700

    add preventDefault

--- resources/views/Sales/formKunjungan.blade.php
+++ resources/views/Sales/formKunjungan.blade.php
@@ -167,9 +167,9 @@
     $('.multi-field-wrapper').each(function() {         
         var $wrapper = $('.multi-fields', this);
         $(".add-field", $(this)).click(function(e) {
-            // $('.multi-field:first-child', $wrapper).clone(true).appendTo($wrapper).find('#produk').val('').focus();
-            $('.multi-field:first-child', $wrapper).clone(true).appendTo($wrapper);
+            e.preventDefault();            
             alert("OK");
+            $('.multi-field:first-child', $wrapper).clone(true).appendTo($wrapper);
         });
         $('.multi-field .remove-field', $wrapper).click(function() {
             if ($('.multi-field', $wrapper).length > 1)
