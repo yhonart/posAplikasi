2db0b497622c010893511b0f7ba34f322fdc8faa
yhONdev <yusuf7789@gmail.com>
2025-07-06 10:29:12 +0700

    change warning

--- resources/views/Z_Additional_Admin/AdminConfig/ConfigCustomerDelivery.blade.php
+++ resources/views/Z_Additional_Admin/AdminConfig/ConfigCustomerDelivery.blade.php
@@ -38,6 +38,11 @@
                             <div class="col-md-4">
                                 <button type="submit" class="btn btn-success btn-sm font-weight-bold">Simpan</button>
                             </div>
+                            <div class="col-md-4">
+                                <button type="button" class="btn btn-warning btn-sm font-weight-bold" data-dismiss="modal" aria-label="Close">
+                                    Batal
+                                </button>
+                            </div>
                         </div>
                     </div>
                 </div>
