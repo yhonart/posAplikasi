f5e483a9e0ce2860dd7c3c42980cb430604f5832
yhONdev <yusuf7789@gmail.com>
2025-07-08 08:34:56 +0700

    add button and delete

--- resources/views/Z_Additional_Admin/AdminConfig/ConfigCustomerPenjualan.blade.php
+++ resources/views/Z_Additional_Admin/AdminConfig/ConfigCustomerPenjualan.blade.php
@@ -5,28 +5,38 @@
                 <h3 class="card-title">Pengaturan Penjualan Pelanggan</h3>
             </div>
             <div class="card-body">
-                <table class="table table-sm table-valign-middle hover">
-                    <thead>
-                        <tr>
-                            <th>Kode Produk</th>
-                            <th>Nama Produk</th>
-                            <th>Qty.Order</th>
-                            <th></th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        @foreach($customerOrder as $cor)
-                            <tr>
-                                <td>{{$cor->product_code}}</td>
-                                <td>{{$cor->product_name}}</td>
-                                <td>{{$cor->qty_order}}</td>
-                                <td class="text-right">
-                                    <button type="button" class="btn btn-sm btn-primary"><i class="fa-solid fa-pencil"></i></button>
-                                </td>
-                            </tr>
-                        @endforeach
-                    </tbody>
-                </table>
+                <div class="row">
+                    <div class="col-md-12">
+                        <button type="button" class="btn btn-sm btn-warning">Tambah Order</button>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-12">
+                        <table class="table table-sm table-valign-middle hover">
+                            <thead>
+                                <tr>
+                                    <th>Kode Produk</th>
+                                    <th>Nama Produk</th>
+                                    <th>Qty.Order</th>
+                                    <th></th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                @foreach($customerOrder as $cor)
+                                    <tr>
+                                        <td>{{$cor->product_code}}</td>
+                                        <td>{{$cor->product_name}}</td>
+                                        <td>{{$cor->qty_order}}</td>
+                                        <td class="text-right">
+                                            <button type="button" class="btn btn-sm btn-primary"><i class="fa-solid fa-pencil"></i></button>
+                                            <button type="button" class="btn btn-sm btn-danger"><i class="fa-solid fa-xmark"></i></button>
+                                        </td>
+                                    </tr>
+                                @endforeach
+                            </tbody>
+                        </table>
+                    </div>
+                </div>                
             </div>
         </div>
     </div>
