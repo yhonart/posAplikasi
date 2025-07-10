257d4fa9703eb97c05f5f7f390db38654cedc4e2
yhONdev <yusuf7789@gmail.com>
2025-07-10 16:12:51 +0700

    remove table

--- resources/views/DeliveryJob/tableSchedule.blade.php
+++ resources/views/DeliveryJob/tableSchedule.blade.php
@@ -1,43 +1,34 @@
 <?php
     $nomor = 1;
 ?>
-<table class="table table-sm table-valign-middle table-hover table-responsive">
-    <thead>
-        <tr>
-            <th>Pelanggan</th>
-            <th>Produk</th>
-        </tr>
-    </thead>
-    <tbody>
-        </tbody>
-    </table>
-    @foreach($listPengiriman as $lpr)
-        <div class="card border border-1 border-info shadow-sm">
-            <div class="card-header">
-                <h3 class="card-title font-weight-bold">{{$lpr->customer_store}}</h3>
-            </div>
-            <div class="card-body">
-                <div class="row">
-                    <div class="col-md-4">
-                        <span class="font-weight-bold">Alamat :</span> <br>
-                        <small>{{$lpr->address}}</small>
-                    </div>
-                    <div class="col-md-4">
-                        <span class="font-weight-bold">Produk</span> <br>
-                        @foreach($getProductOrder as $gpo)
-                            @if($gpo->customer_code == $lpr->customer_code)
-                                <ul>
-                                    <li>{{$gpo->product_name}} : {{$gpo->qty_order}} Pcs.</li>
-                                </ul>
-                            @endif
-                        @endforeach
-                        <br>
-                    </div>
-                    <div class="col-md-4">
-                        <button type="button" class="btn btn-info btn-sm rounded-pill">Diterima</button>
-                        <button type="button" class="btn btn-danger btn-sm rounded-pill">Pending</button>
-                    </div>
+
+@foreach($listPengiriman as $lpr)
+    <div class="card border border-1 border-info shadow-sm">
+        <div class="card-header">
+            <h3 class="card-title font-weight-bold">{{$lpr->customer_store}}</h3>
+        </div>
+        <div class="card-body">
+            <div class="row">
+                <div class="col-md-4">
+                    <span class="font-weight-bold">Alamat :</span> <br>
+                    <small>{{$lpr->address}}</small>
+                </div>
+                <div class="col-md-4">
+                    <span class="font-weight-bold">Produk</span> <br>
+                    @foreach($getProductOrder as $gpo)
+                        @if($gpo->customer_code == $lpr->customer_code)
+                            <ul>
+                                <li>{{$gpo->product_name}} : {{$gpo->qty_order}} Pcs.</li>
+                            </ul>
+                        @endif
+                    @endforeach
+                    <br>
+                </div>
+                <div class="col-md-4">
+                    <button type="button" class="btn btn-info btn-sm rounded-pill">Diterima</button>
+                    <button type="button" class="btn btn-danger btn-sm rounded-pill">Pending</button>
                 </div>
             </div>
-        </div>        
-    @endforeach
+        </div>
+    </div>        
+@endforeach
