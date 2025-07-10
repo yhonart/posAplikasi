b8d6fa273b674fa14b5c15d9952baefaa0a09156
yhONdev <yusuf7789@gmail.com>
2025-07-10 14:44:09 +0700

    add td product

--- resources/views/DeliveryJob/tableSchedule.blade.php
+++ resources/views/DeliveryJob/tableSchedule.blade.php
@@ -12,12 +12,15 @@
     <tbody>
         @foreach($listPengiriman as $lpr)
             <tr>
-                <td class="text-right">
+                <td class="text-left">
                     <button type="button" class="btn btn-sm btn-flat btn-success"><i class="fa-solid fa-circle-check"></i></button>
                 </td>
                 <td>
                     <b>{{$lpr->customer_store}}</b> <br>
                     <small class="text-muted">Alamat : {{$lpr->address}}</small>
+                </td>
+                <td>
+                    
                 </td>
             </tr>
         @endforeach
