d1f999292a88e2ec1da1140ac138ddd0c4535352
yhONdev <yusuf7789@gmail.com>
2025-07-10 14:17:16 +0700

    remove card

--- resources/views/DeliveryJob/mainDelivery.blade.php
+++ resources/views/DeliveryJob/mainDelivery.blade.php
@@ -1,20 +1,13 @@
 <div class="row justify-content-center">
     <div class="col-md-8">
-        <div class="card card-blue">
-            <div class="card-header">
-                <h3 class="card-title">Schedule</h3>
+        <div class="row">
+            <div class="col-md-3">
+                <input type="text" class="form-control form-control-sm datetimepicker-input" name="fromDate" id="fromDate">
             </div>
-            <div class="card-body">
-                <div class="row">
-                    <div class="col-md-3">
-                        <input type="text" class="form-control form-control-sm datetimepicker-input" name="fromDate" id="fromDate">
-                    </div>
-                </div>
-                <div class="row">
-                    <div class="col-md-12">
-                        <div id="contentSchedule"></div>
-                    </div>
-                </div>
+        </div>
+        <div class="row">
+            <div class="col-md-12">
+                <div id="contentSchedule"></div>
             </div>
         </div>
     </div>
