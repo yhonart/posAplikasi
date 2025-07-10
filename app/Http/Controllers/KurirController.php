3e663543992fd17d703152f99f4b58b19824b764
yhONdev <yusuf7789@gmail.com>
2025-07-10 13:36:06 +0700

    change funcDate

--- app/Http/Controllers/KurirController.php
+++ app/Http/Controllers/KurirController.php
@@ -3,14 +3,29 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Auth;
+use Carbon\Carbon;
 
 class KurirController extends Controller
 {
-    public function mainKurir(){
+    public function mainKurir(){        
         return view('DeliveryJob/mainDelivery');
     }
 
     public function funcDate ($date){
-        echo $date;
+        $today = Carbon::now()->dayOfWeekIso;
+        // $selectedDay = $request->input('day', $today);
+        $dayNames = [
+            1 => 'Senin',
+            2 => 'Selasa',
+            3 => 'Rabu',
+            4 => 'Kamis',
+            5 => 'Jumat',
+            6 => 'Sabtu',
+            7 => 'Minggu',
+        ];
+        
+        echo $today;
     }
 }
--- resources/views/DeliveryJob/mainDelivery.blade.php
+++ resources/views/DeliveryJob/mainDelivery.blade.php
@@ -1,5 +1,5 @@
 <div class="row">
-    <div class="col-md-12">
+    <div class="col-md-8">
         <div class="card card-blue">
             <div class="card-header">
                 <h3 class="card-title">Schedule</h3>
