5f5561e249e1210a9b58bf9eac0de1fba8502a04
yhonart <yusuf7789@gmail.com>
2025-07-11 22:36:59 +0700

    change fixing witch copilot

--- app/Http/Controllers/KurirController.php
+++ app/Http/Controllers/KurirController.php
@@ -12,7 +12,31 @@ class KurirController extends Controller
     public function mainKurir(){        
         return view('DeliveryJob/mainDelivery');
     }
+    public function tableKurir(Request $request){
+        $today = Carbon::now()->dayOfWeekIso; // Get the current day of the week
+        $selectedDay = $request->input('day', $today); // Get the selected day from the request, default to today
 
+        $dayNames = [
+            1 => 'Senin',
+            2 => 'Selasa',
+            3 => 'Rabu',
+            4 => 'Kamis',
+            5 => 'Jumat',
+            6 => 'Sabtu',
+            7 => 'Minggu',
+        ];
+        
+        $hari =  $dayNames[$selectedDay];
+
+        $listPengiriman = DB::table('view_delivery_config')
+            ->where('day_freq',$hari)
+            ->get();
+
+        $getProductOrder = DB::table('view_product_order_customer')
+            ->get();
+
+        return view('DeliveryJob/tableSchedule', compact('listPengiriman','getProductOrder'));        
+    }
     public function funcDate ($date){
         // $today = Carbon::now()->dayOfWeekIso;
         $today = Carbon::parse($date)->dayOfWeekIso; // Use the provided date to determine the day of the week
