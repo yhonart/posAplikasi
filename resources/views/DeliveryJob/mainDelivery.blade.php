6cd47d4d746ab86eab075f5493f7c58765d68c90
yhonart <yusuf7789@gmail.com>
2025-07-11 23:11:30 +0700

    refactor layout in mainDelivery and tableSchedule for improved structure and readability

--- resources/views/DeliveryJob/mainDelivery.blade.php
+++ resources/views/DeliveryJob/mainDelivery.blade.php
@@ -1,11 +1,12 @@
-<div class="row mb-2 justify-content-center">
+<div class="row mb-2 justify-content-center">    
     <div class="col-md-3">
+        <label for="fromDate" class="form-label">Pilih Tanggal</label>
         <input type="text" class="form-control form-control-sm datetimepicker-input" name="fromDate" id="fromDate">
     </div>
 </div>
 <div class="row justify-content-center">
-    <div class="col-md-4 col-12">
-        <div id="contentSchedule"></div>        
+    <div class="col-12"> 
+        <div id="contentSchedule"></div>                
     </div>
 </div>
 <script>
--- resources/views/DeliveryJob/tableSchedule.blade.php
+++ resources/views/DeliveryJob/tableSchedule.blade.php
@@ -1,33 +1,37 @@
 <?php
     $nomor = 1;
 ?>
-@foreach($listPengiriman as $lpr)
-    <div class="card border border-1 border-info shadow-sm">
-        <div class="card-header">
-            <h3 class="card-title font-weight-bold"><i class="fa-solid fa-store"></i> {{$lpr->customer_store}}</h3>
-        </div>
-        <div class="card-body">
-            <div class="row">
-                <div class="col-12">
-                    <span class="font-weight-bold">Alamat :</span> <br>
-                    <small>{{$lpr->address}}</small>
+<div class="row">
+    @foreach($listPengiriman as $lpr)
+    <div class="col-md-4 col-12 mb-3">
+        <div class="card border border-1 border-info shadow-sm">
+            <div class="card-header">
+                <h3 class="card-title font-weight-bold"><i class="fa-solid fa-store"></i> {{$lpr->customer_store}}</h3>
+            </div>
+            <div class="card-body">
+                <div class="row">
+                    <div class="col-12">
+                        <span class="font-weight-bold">Alamat :</span> <br>
+                        <small>{{$lpr->address}}</small>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-12">
+                        <span class="font-weight-bold">Produk</span> <br>
+                        @foreach($getProductOrder as $gpo)
+                            @if($gpo->customer_code == $lpr->customer_code)
+                                <ul>
+                                    <li>{{$gpo->product_name}} : {{$gpo->qty_order}} Pcs.</li>
+                                </ul>
+                            @endif
+                        @endforeach
+                        <br>
+                        <button type="button" class="btn btn-info btn-sm rounded-pill elevation-2 font-weight-bold BTN-OPEN-MODAL-GLOBAL-LG" href="{{route('sales')}}/mainKurir/penerimaan/{{$lpr->delconfig_id}}/{{$lpr->customer_code}}"><i class="fa-solid fa-circle-check"></i> Diterima</button>
+                        <button type="button" class="btn btn-danger btn-sm rounded-pill elevation-2 font-weight-bold BTN-OPEN-MODAL-GLOBAL-LG" href="{{route('sales')}}/mainKurir/pending/{{$lpr->delconfig_id}}"><i class="fa-solid fa-clock-rotate-left"></i> Pending</button>
+                    </div>
                 </div>
             </div>
-            <div class="row">
-                <div class="col-12">
-                    <span class="font-weight-bold">Produk</span> <br>
-                    @foreach($getProductOrder as $gpo)
-                        @if($gpo->customer_code == $lpr->customer_code)
-                            <ul>
-                                <li>{{$gpo->product_name}} : {{$gpo->qty_order}} Pcs.</li>
-                            </ul>
-                        @endif
-                    @endforeach
-                    <br>
-                    <button type="button" class="btn btn-info btn-sm rounded-pill elevation-2 font-weight-bold BTN-OPEN-MODAL-GLOBAL-LG" href="{{route('sales')}}/mainKurir/penerimaan/{{$lpr->delconfig_id}}/{{$lpr->customer_code}}"><i class="fa-solid fa-circle-check"></i> Diterima</button>
-                    <button type="button" class="btn btn-danger btn-sm rounded-pill elevation-2 font-weight-bold BTN-OPEN-MODAL-GLOBAL-LG" href="{{route('sales')}}/mainKurir/pending/{{$lpr->delconfig_id}}"><i class="fa-solid fa-clock-rotate-left"></i> Pending</button>
-                </div>
-            </div>
-        </div>
-    </div>        
-@endforeach
+        </div>        
+    </div>
+    @endforeach
+</div>
