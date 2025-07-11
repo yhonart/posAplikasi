9ae65568304674e1288faa50a1b172e63c1ab4b6
yhonart <yusuf7789@gmail.com>
2025-07-11 23:42:08 +0700

    add Storage and Validator facades to KurirController for enhanced functionality

--- app/Http/Controllers/KurirController.php
+++ app/Http/Controllers/KurirController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Facades\Validator;
 use Carbon\Carbon;
 
 class KurirController extends Controller
