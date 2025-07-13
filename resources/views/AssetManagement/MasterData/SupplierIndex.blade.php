8e80a542c79c1efc60bc45a7ecddedc8da4ca6d6
yhonart <yusuf7789@gmail.com>
2025-07-13 18:56:52 +0700

    refactor layout structure in SupplierIndex view for improved readability

--- resources/views/AssetManagement/MasterData/SupplierIndex.blade.php
+++ resources/views/AssetManagement/MasterData/SupplierIndex.blade.php
@@ -14,14 +14,14 @@
     <div class="container-fluid">            
         <div class="row">
             <div class="col-12">
+                <div class="btn-group btn-block mb-2">
+                    <button class="btn btn-primary font-weight-bold BTN-OPEN-MODAL-GLOBAL-LG" href="{{route('Supplier')}}/AddSupliyer"><i class="fa-solid fa-plus"></i> Supplier</button>
+                    <a href="{{route('Supplier')}}/tableSupplier/downloadExcelSupplier" class="btn btn-success font-weight-bold" target="_blank"><i class="fa-solid fa-file-excel"></i> Download</a>
+                </div>
             </div>
         </div>
         <div class="row mt-2">
-            <div class="col-md-3">
-                <div class="btn-group btn-block mb-2">
-                    <button class="btn btn-primary font-weight-bold BTN-OPEN-MODAL-GLOBAL-LG" href="{{route('Supplier')}}/AddSupliyer">Tambah Supplier</button>
-                    <a href="{{route('Supplier')}}/tableSupplier/downloadExcelSupplier" class="btn btn-success font-weight-bold" target="_blank"><i class="fa-solid fa-file-excel"></i> Download</a>
-                </div>
+            <div class="col-md-3">                
                 <input type="text" name="searchSupplier" id="searchSupplier" class="form-control mb-1 " placeholder="Cari nama supplier" autofocus>
                 <div class="card card-body text-xs  table-responsive p-0" style="height:700px;">
                     @include('Global.global_spinner')
