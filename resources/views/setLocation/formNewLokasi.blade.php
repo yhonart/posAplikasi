f7b7b0ac5b58a3e7c909ac1be4aa4a3dee699add
yhonart <yusuf7789@gmail.com>
2025-08-09 15:22:49 +0700

    adding field form new lokasi

--- resources/views/setLocation/formNewLokasi.blade.php
+++ resources/views/setLocation/formNewLokasi.blade.php
@@ -9,10 +9,40 @@
             <div class="col-md-12">
                 <form id="formNewLokasiToko">
                     <div class="form-group row">
+                        <label for="kodeLokasi" class="col-md-4 text-right">Kode Lokasi</label>
+                        <div class="col-md-4">
+                            <input type="text" name="kodeLokasi" id="kodeLokasi" class="form-control form-control-sm">
+                        </div>
+                    </div>
+                    <div class="form-group row">
                         <label for="namaLokasi" class="col-md-4 text-right">Nama Lokasi</label>
                         <div class="col-md-4">
                             <input type="text" name="namaLokasi" id="namaLokasi" class="form-control form-control-sm">
                         </div>
+                    </div>
+                    <div class="form-group row">
+                        <label for="address" class="col-md-4 text-right">Alamat Lokasi</label>
+                        <div class="col-md-4">
+                            <input type="text" name="address" id="address" class="form-control form-control-sm">
+                        </div>
+                    </div>
+                    <div class="form-group row">
+                        <label for="city" class="col-md-4 text-right">Kota</label>
+                        <div class="col-md-4">
+                            <input type="text" name="city" id="city" class="form-control form-control-sm">
+                        </div>
+                    </div>
+                    <div class="form-group row">
+                        <label for="groupLoc" class="col-md-4 text-right">Group Lokasi</label>
+                        <div class="col-md-4">
+                            <select name="gropuLoc" id="groupLoc" class="form-control form-control-sm">
+                                <option value="3">Toko</option>
+                                <option value="4">Gudang</option>
+                            </select>
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <button type="submit" class="btn btn-sm btn-success" id="btnSimpan">Simpan</button>
                     </div>
                 </form>
             </div>
