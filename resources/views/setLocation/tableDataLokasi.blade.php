377843fa598a3d6167cb03fc1f1b4ee1034749f0
yhonart <yusuf7789@gmail.com>
2025-08-10 16:01:36 +0700

    change header alertify

--- resources/views/setLocation/tableDataLokasi.blade.php
+++ resources/views/setLocation/tableDataLokasi.blade.php
@@ -59,7 +59,7 @@
             },
             function(){
                 alertify.error('Cancel');
-            });
+            }).set({title:"Konfirmasi delete lokasi"});
             
         });
     });
