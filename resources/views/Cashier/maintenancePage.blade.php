8f2920ad0c2664112d85a4e62bd15189250ebe58
yhONdev <yusuf7789@gmail.com>
2025-10-03 22:15:29 +0700

    change sub menu cashier

--- resources/views/Cashier/maintenancePage.blade.php
+++ resources/views/Cashier/maintenancePage.blade.php
@@ -6,22 +6,47 @@
         @if($checkArea <> 0 && $checkGroup == 1)         
             <!-- jika module systemnya hanya kasir dan inventory saja  -->
             @if($module == "AM3")
-            <div class="row mb-2">
-                <div class="col-md-12">
-                    <div class="btn-group">
-                        <a href="{{route('Cashier')}}" class="btn btn-default btn-flat font-weight-bold border-0"><i class="fa-solid fa-boxes-stacked"></i> Kasir</a>
-                        <a href="#" class="btn btn-default btn-flat font-weight-bold border-0"><i class="fa-solid fa-boxes-stacked"></i> Inventory</a>                        
-                        <a href="#" class="btn btn-default btn-flat font-weight-bold border-0"><i class="fa-solid fa-chart-line"></i> Dashboard</a>
+                <div class="row mb-2">
+                    <div class="col-md-12">
+                        <nav class="navbar navbar-expand-lg" style="width:100%;">
+                            <span class="d-flex navbar-brand"><i class="fa-solid fa-chart-line pl-2 text-info"></i></span>
+                            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+                                aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                                <i class="fas fa-ellipsis-h"></i>
+                            </button>
+                            <div class="collapse navbar-collapse" id="navbarSupportedContent">                            
+                                <ul class="nav nav-pills ml-auto" id="main-menu-bar-helpdesk">                                
+                                    <li class="nav-item">
+                                        <a class="nav-link BTN-CLICK" href="{{route('Cashier')}}" data-link="dashPenjualan" data-toggle="tab" id="tabMenuDash">
+                                            KASIR
+                                        </a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a class="nav-link BTN-CLICK" href="#" data-link="displayPembelian" data-toggle="tab" id="tabMenuHistory">
+                                            BRAND
+                                        </a>
+                                    </li>                                                             
+                                    <li class="nav-item">
+                                        <a class="nav-link BTN-CLICK" href="#" data-link="dashHutangPelanggan" data-toggle="tab" id="tabMenuHistory">
+                                            STOCK
+                                        </a>
+                                    </li>                                                             
+                                    <li class="nav-item">
+                                        <a class="nav-link BTN-CLICK" href="#" data-link="dashHutangToko" data-toggle="tab" id="tabMenuHistory">
+                                            DASHBOARD
+                                        </a>
+                                    </li>                                                      
+                                </ul>
+                        </nav>                    
                     </div>
                 </div>
-            </div>
-            <div class="row mb-2">
-                <div class="col-md-12">
-                    <div id="displayInventoryKasir"></div>
+                <div class="row mb-2">
+                    <div class="col-md-12">
+                        <div id="displayLoadInventory"></div>
+                    </div>
                 </div>
-            </div>
             @endif
-            <div class="row">
+            <div class="row" id="displayKasir">
                 <div class="col-12 col-lg-8 pr-0">
                     @include('Global.global_spinner')
                     <div class=" table-responsive text-xs p-0" style="height:700px;">
