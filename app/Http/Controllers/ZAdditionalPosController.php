fe2f8b5598d1c7ca8e4a2f03720885fc6ea49a01
yhonart <yusuf7789@gmail.com>
2025-10-15 08:20:06 +0700

    change controller additional

--- app/Http/Controllers/ZAdditionalPosController.php
+++ app/Http/Controllers/ZAdditionalPosController.php
@@ -10,6 +10,17 @@ use Illuminate\Support\Facades\Hash;
 
 class ZAdditionalPosController extends Controller
 {
+    protected $tempInv;
+    protected $tempKasBesar;
+    protected $TempInventoryController;
+    protected $TempKeuanganController;
+
+    public function __construct(TempInventoryController $tempInv, TempKeuanganController $tempKasBesar)
+    {
+        $this->TempInventoryController = $tempInv;
+        $this->TempKeuanganController = $tempKasBesar;
+    }
+    
     public function checkuserInfo()
     {
         $userID = Auth::user()->id;
@@ -85,6 +96,104 @@ class ZAdditionalPosController extends Controller
     }
 
     public function cariProduk($keyword, $trxNumber){
-        echo $keyword." ".$trxNumber;
+        // echo $keyword." ".$trxNumber;
+        $barcode = "";
+        $username = Auth::user()->name;
+        $company = Auth::user()->company;
+        $cosGroup = 1;
+
+        $getBarcode = DB::table('m_product_unit')
+            ->where('set_barcode', $keyword)
+            ->first();  
+
+        if (!empty($getBarcode)) {
+            $barcode = $getBarcode->set_barcode;
+            $productList = DB::table('view_product_stock');            
+            $productList = $productList->where([
+                ['set_barcode', $keyword],
+                ['comp_id',$company]
+            ]);
+            $productList = $productList->first();
+            $product = $productList->idm_data_product;
+            $unit = $productList->product_satuan;
+            $satuan = $productList->product_size;
+            $priceOrder = $productList->product_price_order;
+            $stock = $productList->stock;
+            $inputStock = $stock - 1;
+            $getPrice = DB::table('m_product_price_sell')
+                    ->where([
+                        ['core_product_price',$product],
+                        ['size_product',$satuan],
+                        ['cos_group',$cosGroup]
+                    ])
+                    ->first();
+                $priceSell = $getPrice->price_sell;
+            $countItem = DB::table('tr_store_prod_list')
+                ->where([
+                    ['from_payment_code', $trxNumber],
+                    ['product_code',$product],
+                    ['unit',$unit],
+                    ['status','>=','1']
+                ])
+                ->count();
+
+            if ($countItem == '0') {
+                DB::table('tr_store_prod_list')
+                    ->insert([
+                        'from_payment_code'=>$trxNumber,
+                        'product_code'=>$product,
+                        'qty'=>'1',
+                        'unit'=>$unit,
+                        'satuan'=>$satuan,
+                        'unit_price'=>$priceSell,
+                        'disc'=>'0',
+                        't_price'=>$priceSell,
+                        'm_price'=>$priceOrder,
+                        'status'=>'1',
+                        'created_by'=>$username,
+                        'stock'=>$inputStock,
+                        'date'=> now(),
+                    ]);
+            }
+            else {
+                $selectTrxItem = DB::table('tr_store_prod_list')
+                    ->where([
+                        ['from_payment_code', $trxNumber],
+                        ['product_code',$product],
+                        ['unit',$unit],
+                        ['status','>=','1']
+                    ])
+                    ->first();
+
+                $lastQty = $selectTrxItem->qty;
+                $sumQty = $lastQty + 1;
+                $lastHargaSatuan = $selectTrxItem->unit_price;
+                $lastTotalHarga = $selectTrxItem->t_price;
+                $updateHarga = $lastTotalHarga + $lastHargaSatuan;
+
+                DB::table('tr_store_prod_list')
+                    ->where([
+                        ['from_payment_code', $trxNumber],
+                        ['product_code',$product],
+                        ['unit',$unit],
+                        ['status','>=','1']
+                    ])
+                    ->update([
+                        'qty'=>$sumQty,
+                        't_price'=>$updateHarga,
+                        'stock'=>$inputStock,
+                    ]);
+                
+            }
+                // Insert into laporan
+            $location = $this->checkuserInfo();
+            $prodQty = '1';
+            $description = "Penjualan ".$username;                
+            $this->TempInventoryController->reportBarangKeluar($product, $satuan, $location, $prodQty, $description, $trxNumber, $username);
+            $this->penguranganStock($product, $location, $satuan, $prodQty);
+        }
+        else {
+            # code...
+        }
     }
 }
