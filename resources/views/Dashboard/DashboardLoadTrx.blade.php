65d774c0372ce2a4688db247e2bebd5f89a1e2a0
yhonart <yusuf7789@gmail.com>
2026-06-29 21:23:41 +0700

    change remove notification into home

--- resources/views/Dashboard/DashboardLoadTrx.blade.php
+++ resources/views/Dashboard/DashboardLoadTrx.blade.php
@@ -3,17 +3,6 @@
     $dayNoww = date('l', strtotime($hariIni));
     $strHari = "Monday";
 ?>
-@if($dayNoww == $strHari AND $getInfoKas == '0')
-<div class="row mb-2">
-    <div class="col-md-12">
-        <div class="callout callout-info">
-            <h5><img src="{{asset('public/images/Animation1738512688097.gif')}}" height="90" width="90"> Udah Hari <b>Senin</b> nih!</h5>
-            <p>Jangan lupa masukkan Reimbursement / Pengembalian Dana Kas, untuk kebutuhan kas kecil anda. Klik link dibawah ini untuk memasukkan dana : </p>
-            <a href="{{route('trxReumbers')}}" class="text-info font-weight-bold">Reimbursement / Pengembalian Dana Kas</a>
-        </div>        
-    </div>
-</div>
-@endif
 <div class="row mb-2">
     <div class="col-lg-3 col-6">
         <a href="#" class="onClick-Data" data-condition="alltrx" data-from="{{$fromDate}}" data-end="{{$endDate}}">
